37cb9751c87f283976c997dec68ab382468c7162
chen.rubin <chenrubin123@gmail.com>
2019-10-29 20:12:14 +0200

    add ds

---
+++ ds/lib/bit_array.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <math.h>
+#include "barr.h"
+
+#define BYTE 8
+#define BITS_IN_ARRAY (sizeof(size_t) * BYTE)
+
+
+bitarray BArrSetAll(bitarray arr)
+{
+	return ((size_t)(pow(2, BITS_IN_ARRAY) - 1) | arr);
+}
+
+bitarray BArrResetAll(bitarray arr)
+{
+	return (arr & 0);
+}
+
+bitarray BArrSetBit(bitarray arr, size_t loc, int mod)
+{
+	if (mod)
+	{
+		return (arr | (1 << loc));
+	}
+	else
+	{
+		return arr & (~(1 << loc));	
+	}
+}
+
+bitarray BArrSetOn(bitarray arr, size_t loc)
+{
+	return (arr | ((size_t)1 << loc));
+}
+
+bitarray BArrSetOff(bitarray arr, size_t loc)
+{
+	return (arr & ~((size_t)1 << loc));
+}
+
+int BArrIsOn(bitarray arr, size_t loc)
+{
+	return (((1 << loc) & arr) ? 1 : 0);
+}
+
+int BArrIsOff(bitarray arr, size_t loc)
+{
+	return (((1 << loc) & arr) ? 0 : 1);
+}
+
+size_t BArrCountOn(bitarray arr)
+{
+	size_t i = 0;
+	size_t counter = 0;
+	size_t mask = 1;
+	
+	for (i = 0; i < BITS_IN_ARRAY; ++i)
+	{
+		if (mask & arr)
+		{
+			++counter;
+		}
+		
+		mask <<= mask;
+	}
+	
+	return counter;
+}
+
+size_t BArrCountOff(bitarray arr)
+{
+	return ((sizeof(size_t) * 8) - BArrCountOn(arr));
+}
+
+bitarray BArrRotR(bitarray arr, size_t num)
+{
+	int num_of_rot = num % BITS_IN_ARRAY;
+	
+	return ((arr >> num_of_rot) | 
+			(arr << (BITS_IN_ARRAY - num_of_rot)));
+}
+
+bitarray BArrRotL(bitarray arr, size_t num)
+{
+	int num_of_rot = num % BITS_IN_ARRAY;
+	
+	return ((arr << num_of_rot) | 
+			(arr >> (BITS_IN_ARRAY - num_of_rot)));
+}
+
+bitarray BArrMirror(bitarray arr)
+{
+	bitarray res = 0;
+	size_t i = 0;
+	
+	for (i = 0; i < BITS_IN_ARRAY; ++i)
+	{
+		res = res << 1;
+		res = res | (arr & 1);
+		arr = arr >> 1;
+	}
+	
+	return res;
+}
+
+char* BArrToString(char* buffer ,bitarray arr)
+{
+	size_t i = 0;
+	int bit_mask = 1;
+/*	char buf[2];*/
+	
+	for (i = 0; i < BITS_IN_ARRAY; ++i)
+	{
+		sprintf(/*buf*/buffer, "%d", (arr & bit_mask));
+/*		strcat(buffer, buf);*/
+	}
+	
+	return buffer;
+}
+
+bitarray BArrFlip(bitarray arr, size_t loc)
+{
+	return (arr ^ (loc << 1));
+}
---
+++ ds/lib/main.c
@@ -0,0 +1,241 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include "barr.h"
+
+
+#define BYTE 8
+#define BITS_IN_ARRAY (sizeof(size_t) * BYTE)
+#define BIT_X_SET(X) ((size_t)1 << (X))
+
+#define PRINTTESTRESULTS(func,num, res) \
+(printf("Function %s: Test %d %s\n",func, num, (res) == 1 ? "passed" : "failed"))\
+
+void TestBArrSetAll();
+void TestBArrResetAll();
+void TestBArrSetBit();
+void TestBArrSetOn();
+void TestBArrSetOff();
+void TestBArrIsOn();
+void TestBArrIsOff();
+void TestBArrCountOn();
+void TestBArrCountOff();
+void TestBArrRotR();
+void TestBArrRotL();
+void TestBArrMirror();
+void TestBArrToString();
+void TestBArrFlip();	
+	
+int main()
+{
+	TestBArrSetAll();
+	TestBArrResetAll();
+	TestBArrSetBit();
+	TestBArrSetOn();
+	TestBArrSetOff();
+	TestBArrIsOn();
+	TestBArrIsOff();
+	TestBArrCountOn();
+	TestBArrCountOff();
+	TestBArrRotR();
+	TestBArrRotL();
+	TestBArrMirror();
+	TestBArrToString();
+	TestBArrFlip();	
+	
+	return 0;
+}
+
+void TestBArrSetAll()
+{
+	size_t num1 = 0xFFFFFFFFFFFFFFFF;
+	size_t num2 = 0;
+	size_t num3 = 0x0123456789ABCDEF;
+	
+	PRINTTESTRESULTS("BArrSetAll", 1, ((pow(2, BITS_IN_ARRAY) - 1) == 
+									 ((double)BArrSetAll(num1))));
+									 
+	PRINTTESTRESULTS("BArrSetAll", 2, ((pow(2, BITS_IN_ARRAY) - 1) == 
+									 ((double)BArrSetAll(num2))));
+								
+	PRINTTESTRESULTS("BArrSetAll", 3, ((pow(2, BITS_IN_ARRAY) - 1) == 
+									 ((double)BArrSetAll(num3))));			 
+}
+
+void TestBArrResetAll()
+{
+	size_t num1 = 0xFFFFFFFFFFFFFFFF;
+	size_t num2 = 0;
+	size_t num3 = 0x0123456789ABCDEF;
+	
+	PRINTTESTRESULTS("BArrResetAll",1, (BArrResetAll(num1) == 0));
+	PRINTTESTRESULTS("BArrResetAll",2, (BArrResetAll(num2) == 0));
+	PRINTTESTRESULTS("BArrResetAll",3, (BArrResetAll(num3) == 0));
+}
+
+void TestBArrSetBit()
+{
+	size_t num = 0x0123456789ABCDEF;
+	
+	PRINTTESTRESULTS("BArrSetBit",1, ((num | BIT_X_SET(6)) == 
+									 BArrSetBit(num, 6, 1)));
+	PRINTTESTRESULTS("BArrSetBit",2, ((num | BIT_X_SET(56)) == 
+									 BArrSetBit(num, 56, 1)));
+	PRINTTESTRESULTS("BArrSetBit",3, ((num & ~BIT_X_SET(62)) == 
+									 BArrSetBit(num, 62, 0)));
+	PRINTTESTRESULTS("BArrSetBit",4, (num & ~BIT_X_SET(5)) == 
+									 BArrSetBit(num, 5, 0));									 				
+}
+
+void TestBArrSetOn()
+{
+	size_t num = 0x0123456789ABCDEF;
+	
+	PRINTTESTRESULTS("BArrSetOn",1, (num | BIT_X_SET(6)) == BArrSetOn(num, 6));
+	PRINTTESTRESULTS("BArrSetOn",2, (num | BIT_X_SET(56)) == BArrSetOn(num, 56));
+	PRINTTESTRESULTS("BArrSetOn",3, (num | BIT_X_SET(62)) == BArrSetOn(num, 62));
+	PRINTTESTRESULTS("BArrSetOn",4, (num | BIT_X_SET(4)) == BArrSetOn(num, 4));
+}
+
+void TestBArrSetOff()
+{
+	size_t num = 0x0123456789ABCDEF;
+	
+	PRINTTESTRESULTS("BArrSetOff",1, (num & ~BIT_X_SET(6)) == BArrSetOff(num, 6));
+	PRINTTESTRESULTS("BArrSetOff",2, (num & ~BIT_X_SET(56)) == BArrSetOff(num, 56));
+	PRINTTESTRESULTS("BArrSetOff",3, (num & ~BIT_X_SET(62)) == BArrSetOff(num, 62));
+	PRINTTESTRESULTS("BArrSetOff",4, (num & ~BIT_X_SET(4)) == BArrSetOff(num, 4));
+}
+
+void TestBArrIsOn()
+{
+	size_t num = 0x0123456789ABCDEF;
+	
+	PRINTTESTRESULTS("BArrIsOn",1, BArrIsOn(num, 0));
+	PRINTTESTRESULTS("BArrIsOn",2, BArrIsOn(num, 63));
+	PRINTTESTRESULTS("BArrIsOn",3, !(BArrIsOn(num, 18)));
+	PRINTTESTRESULTS("BArrIsOn",4, !(BArrIsOn(num, 59)));
+}
+
+void TestBArrIsOff()
+{
+	size_t num = 0x0123456789ABCDEF;
+	
+	PRINTTESTRESULTS("BArrIsOff",1, !BArrIsOff(num, 0));
+	PRINTTESTRESULTS("BArrIsOff",2, !BArrIsOff(num, 63));
+	PRINTTESTRESULTS("BArrIsOff",3, BArrIsOff(num, 18));
+	PRINTTESTRESULTS("BArrIsOff",4, BArrIsOff(num, 59));
+}
+
+void TestBArrCountOn()
+{
+	size_t num1 = 0xFFFFFFFFFFFFFFFF;
+	size_t num2 = 0;
+	size_t num3 = 0x0123456789ABCDEF;
+	
+	PRINTTESTRESULTS("BArrCountOn",1, 64 == BArrCountOn(num1));
+	PRINTTESTRESULTS("BArrCountOn",2, 0 == BArrCountOn(num2));
+	PRINTTESTRESULTS("BArrCountOn",3, 32 == BArrCountOn(num3));
+}
+
+void TestBArrCountOff()
+{
+	size_t num1 = 0xFFFFFFFFFFFFFFFF;
+	size_t num2 = 0;
+	size_t num3 = 0x0123456709ABCDEF;
+	
+	PRINTTESTRESULTS("BArrCountOff",1, 0 == BArrCountOff(num1));
+	PRINTTESTRESULTS("BArrCountOff",2, 64 == BArrCountOff(num2));
+	PRINTTESTRESULTS("BArrCountOff",3, 25 == BArrCountOff(num3));
+}
+
+void TestBArrRotR()
+{
+	size_t num = 0x8123456789ABCDEF;
+	size_t num_rot1 = 0x7BE048D159E26AF3;
+	size_t num_rot2 = 0xBCDEF8123456789A;
+	size_t num_rot3 = 0xDEF8123456789ABC;
+	
+	PRINTTESTRESULTS("BArrRotR",1, num_rot1 == BArrRotR(num, 10));
+	PRINTTESTRESULTS("BArrRotR",2, num_rot2 == BArrRotR(num, 20));
+	PRINTTESTRESULTS("BArrRotR",3, num_rot3 == BArrRotR(num, 78));
+}
+
+void TestBArrRotL()
+{
+	size_t num = 0x8123456789ABCDEF;
+	size_t num_rot1 = 0x8D159E26AF37BE04;
+	size_t num_rot2 = 0xD159E26AF37BE048;
+	size_t num_rot3 = 0x56789ABCDEF81234;
+	
+	PRINTTESTRESULTS("BArrRotL",1, num_rot1 == BArrRotL(num, 10));
+	PRINTTESTRESULTS("BArrRotL",2, num_rot2 == BArrRotL(num, 20));
+	PRINTTESTRESULTS("BArrRotL",3, num_rot3 == BArrRotL(num, 78));
+}
+
+void TestBArrMirror()
+{
+	size_t num1 = 0xFFFFFFFFFFFFFFFF;
+	size_t num2 = 0;
+	size_t num3 = 0xF123456789ABCDEF;
+	size_t num3_mirror = 0x13579BDFE2468ACF;
+	
+	PRINTTESTRESULTS("BArrMirror",1, num1 == BArrMirror(num1));
+	PRINTTESTRESULTS("BArrMirror",2, num2 == BArrMirror(num2));
+	PRINTTESTRESULTS("BArrMirror",3, num3_mirror == BArrMirror(num3));
+}
+
+void TestBArrToString()
+{
+	size_t num1 = 0xFFFFFFFFFFFFFFFF;
+	size_t num2 = 0;
+	size_t num3 = 0xF123456789ABCDEF;
+	char *buf = (char *)malloc(BITS_IN_ARRAY + 1);
+	
+	PRINTTESTRESULTS("BArrToString",1, !(strcmp("11111111111111111111111111111\
+		 11111111111111111111111111111111111",BArrToString(buf, num1))));
+	PRINTTESTRESULTS("BArrToString",2, !(strcmp("00000000000000000000000000000\
+		 00000000000000000000000000000000000", BArrToString(buf, num2))));
+	PRINTTESTRESULTS("BArrToString",3, !(strcmp("11110001001000110100010101100\
+		 11110001001101010111100110111101111", BArrToString(buf, num3))));
+/*	
+	if (!(strcmp("11111111111111111111111111111\
+		 11111111111111111111111111111111111",BArrToString(buf, num1))))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+	
+	if (!(strcmp("00000000000000000000000000000\
+		 00000000000000000000000000000000000", BArrToString(buf, num2))))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+	
+	if (!(strcmp("11110001001000110100010101100\
+		 11110001001101010111100110111101111", BArrToString(buf, num3))))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}*/
+}
+
+void TestBArrFlip()
+{
+	size_t num = 0xF123456789ABCDEF;
+	
+	PRINTTESTRESULTS("BArrFlip",1 ,0xF123456789AACDEF == BArrFlip(num, 16));
+	PRINTTESTRESULTS("BArrFlip",2 ,0xF127456789ABCDEF == BArrFlip(num, 50));
+	PRINTTESTRESULTS("BArrFlip",3 ,0xF123456F89ABCDEF == BArrFlip(num, 35));
+}
