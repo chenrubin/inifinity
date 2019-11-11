f43d550301d9ce0f21674d04b770d916a7e06bd7
chen.rubin <chenrubin123@gmail.com>
2019-11-11 09:15:38 +0200

    update easyCreateFiles.sh

--- chen/easyCreateFiles.sh
+++ chen/easyCreateFiles.sh
@@ -13,24 +13,12 @@ echo "*									  *" >> $1.c
 echo -e "************************************/\n" >> $1.c
 
 echo -e "#include <stdio.h> /* printf */\n" >> $1.c
-echo -e "#include \"$1.h\"\n" >> $1.c
+echo "#include \"$1.h\"" >> $1.c
+echo -e "#include \"MyUtils.h\" /* MAX2,MIN2 */\n" >> $1.c
 
 echo -e "#include <stdio.h> /* printf */\n" > $1_tests.c
-echo -e "#include \"$1.h\"\n" >> $1_tests.c
-
-echo "#define KNRM  \"\x1B[0m\"" >> $1_tests.c
-echo "#define KRED  \"\x1B[31m\"" >> $1_tests.c
-echo "#define KGRN  \"\x1B[32m\"" >> $1_tests.c
-echo "#define KYEL  \"\x1B[33m\"" >> $1_tests.c
-echo "#define KBLU  \"\x1B[34m\"" >> $1_tests.c
-echo "#define KMAG  \"\x1B[35m\"" >> $1_tests.c
-echo "#define KCYN  \"\x1B[36m\"" >> $1_tests.c
-echo "#define KWHT  \"\x1B[37m\"" >> $1_tests.c
-echo "" >> $1_tests.c
-
-echo "#define PRINTTESTRESULTS(func,num, res)\\" >> $1_tests.c
-echo "(printf("%-55s: Test %d %s\\n" KNRM,func, num, (res) == 1 ?\\" >> $1_tests.c
-echo "KGRN "\\t\\tpassed" : KRED "\\t\\tfailed"))" >> $1_tests.c
+echo "#include \"$1.h\"" >> $1_tests.c
+echo -e "#include \"MyUtils.h\" /* MAX2,MIN2 */\n" >> $1_tests.c
 
 echo -e "\n\nint main()" >> $1_tests.c
 echo -e "{\n\n" >> $1_tests.c
