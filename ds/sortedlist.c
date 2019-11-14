51821d18320ca7509a7b27d61c00e01a87f0fce7
tamir.dayan <tamirda@gmail.com>
2019-11-14 18:56:00 +0200

    update sorted list

--- ds/sortedlist.c
+++ ds/sortedlist.c
@@ -1,7 +1,7 @@
 /************************************
 *		Author: ChenR				  *
 *		Reviewer: 					  *
-*		sortedlist							  *
+*		sortedlist					  *
 *		7/11/2019					  *
 *									  *
 ************************************/
@@ -9,6 +9,7 @@
 #include <stdio.h> /* printf */
 #include <assert.h> /* assert */
 #include <stdlib.h> /* malloc */
+#include <assert.h> /* assert */
 
 #include "dllist.h"
 #include "sortedlist.h"
@@ -21,10 +22,13 @@ struct srt_list
 	void *is_before_param;
 };
 
-struct srt_iterator
+typedef struct wrapper
 {
-	dll_iter_t iterator;
-};
+	void *param;
+	action_func_t ptr;
+} structWrapper;
+
+int ForEachActionWrapper(dll_node_t *node, void *param);
 
 srt_list_t *SrtListCreate(void *param, is_before_t ptr)
 {
@@ -32,7 +36,7 @@ srt_list_t *SrtListCreate(void *param, is_before_t ptr)
 	if (NULL == new_srtlist)
 	{
 		return NULL;
-	}	
+	}
 	
 	new_srtlist -> dll = DLListCreate();
 	if (NULL == new_srtlist -> dll)
@@ -52,6 +56,8 @@ srt_list_t *SrtListCreate(void *param, is_before_t ptr)
 
 void SrtListDestroy(srt_list_t *list)
 {
+	assert(list);
+	
 	DLListDestroy(list -> dll);
 	list -> is_before_ptr = NULL;
 	list -> is_before_param = NULL;
@@ -61,26 +67,188 @@ void SrtListDestroy(srt_list_t *list)
 srt_iter_t SrtListInsert(void *data, srt_list_t *list)
 {
 	dll_node_t *node = NULL;
-	srt_iter_t srt_iter;
+	srt_iter_t srt_iter = {0};
+	
+	assert(list);
 	
 	for (node = DLListBegin(list -> dll);
 		 node != DLListEnd(list -> dll);
 		 node = DLListNext(node))
 	{
-		if (1 == (list -> is_before_ptr(data, DLListGetData(node), list -> is_before_param)))
+		if (1 == (list -> is_before_ptr(data, DLListGetData(node), 
+				  list -> is_before_param)))
 		{
 			break;
 		}
 	}
 	
-	DLListInsert(data, node, list -> dll);
-	srt_iter.iterator = node;
+	srt_iter.dll_iterator = DLListInsert(data, node, list -> dll);
 	
 	return srt_iter;
 }
 
+int MyForEachAction(dll_node_t *node, void *wrap)
+{
+	int status = 0;
+	
+	status = (wrap -> ptr)(, wrap -> param);
+}
 
+int SrtListForEach(srt_iter_t begin, srt_iter_t end, void *param, 
+				   action_func_t ptr)
+{
+	structWrapper *wrap_struct = (structWrapper *)malloc(sizeof(structWrapper));
+	if (NULL == wrap_struct)
+	{
+		return 1;
+	}
+	
+	assert(begin.dll_iterator);
+	assert(end.dll_iterator);
+	assert(ptr);
+	
+	wrap_struct -> ptr = ptr;
+	wrap_struct -> param = param;
+	
+	return (DLListForEach(begin.dll_iterator, end.dll_iterator, 
+				  /*param*/wrap_struct, /*ptr*/MyForEachAction));
+/*	
+	for (srt_iter = begin; srt_iter.dll_iterator != end.dll_iterator; 
+		 srt_iter = SrtListNext(srt_iter))
+	{
+		ptr(SrtListGetData(srt_iter), param);
+	}
+*/	
+}
 
+srt_iter_t SrtListBegin(srt_list_t *list)
+{
+	srt_iter_t srt_iter = {0};
+	
+	assert(list);
+	
+	srt_iter.dll_iterator = DLListBegin(list -> dll);
+	
+	return srt_iter;
+}
 
+srt_iter_t SrtListEnd(srt_list_t *list)
+{
+	srt_iter_t srt_iter = {0};
+	
+	assert(list);
+	
+	srt_iter.dll_iterator = DLListEnd(list -> dll);
+	
+	return srt_iter;
+}
 
+srt_iter_t SrtListNext(srt_iter_t iterator)
+{
+	srt_iter_t srt_iter = {0};
+	
+	assert(iterator.dll_iterator);
+	
+	srt_iter.dll_iterator = DLListNext(iterator.dll_iterator);
+	
+	return srt_iter;
+}
 
+srt_iter_t SrtListPrev(srt_iter_t iterator)
+{
+	srt_iter_t srt_iter = {0};
+	
+	assert(iterator.dll_iterator);
+	
+	srt_iter.dll_iterator = DLListPrev(iterator.dll_iterator);
+	
+	return srt_iter;
+}
+
+void *SrtListGetData(srt_iter_t iterator)
+{
+	assert(iterator.dll_iterator);
+	
+	return (DLListGetData(iterator.dll_iterator));
+}
+
+int SrtListIsSameIterator(srt_iter_t iterator1, srt_iter_t iterator2)
+{
+	assert(iterator1.dll_iterator);
+	assert(iterator2.dll_iterator);
+	
+	return (iterator1.dll_iterator == iterator2.dll_iterator);
+}
+
+void *SrtListPopFront(srt_list_t *list)
+{
+	assert(list);
+	
+	return (DLListPopFront(list -> dll));
+}
+
+void *SrtListPopBack(srt_list_t *list)
+{
+	assert(list);
+	
+	return (DLListPopBack(list -> dll));
+}
+
+size_t SrtListSize(const srt_list_t *list)
+{
+	assert(list);
+	
+	return (DLListSize(list -> dll));
+}
+
+srt_iter_t SrtListRemove(srt_iter_t iterator)
+{
+	srt_iter_t srt_iter = {0};
+
+	assert(iterator.dll_iterator);
+		
+	srt_iter.dll_iterator = DLListRemove(iterator.dll_iterator);
+	
+	return srt_iter;
+}
+
+int SrtListIsEmpty(const srt_list_t *list)
+{
+	assert(list);
+	
+	return (DLListIsEmpty(list -> dll));
+}
+
+void SrtListMerge(srt_list_t *src_list, srt_list_t *dest_list)
+{
+	srt_iter_t srt_iter = {0};
+	
+	assert(src_list);
+	assert(dest_list);
+	
+	for (srt_iter = SrtListBegin(src_list); 
+		 !SrtListIsSameIterator(srt_iter, SrtListEnd(src_list));
+		 srt_iter = SrtListNext(srt_iter))
+	{
+		SrtListInsert(SrtListGetData(srt_iter), dest_list);
+	}
+	
+	SrtListDestroy(src_list);
+}
+
+/*******************************************************/
+
+/*	
+srt_iter_t SrtListFindIf(srt_iter_t begin, srt_iter_t end, 
+						 const void *param, is_match_func_t ptr)
+{
+	srt_iter_t srt_iter = {0};
+	assert(begin.dll_iterator);
+	assert(end.dll_iterator);
+	assert(ptr);
+	
+	srt_iter.dll_iterator = DLListFind(begin.dll_iterator, end.dll_iterator, 
+					  param, ptr));
+					  
+	return srt_iter;				  
+}*/					  
--- ds/sortedlist_tests.c
+++ ds/sortedlist_tests.c
@@ -1,4 +1,5 @@
 #include <stdio.h> /* printf */
+#include <string.h>
 
 #include "sortedlist.h"
 #include "dllist.h"
@@ -6,25 +7,28 @@
 
 void TestCreateDestroy();
 void TestInsert();
+void TestPopFrontSize();
+void TestPopBackSize();
+void TestRemove();
+void TestMerge();
+void TestForEach();
+void TestFindIf();
 int IsMatch(const void *data1, const void *data2, void *param);
-
-struct srt_list
-{
-	dl_list_t *dll;
-	is_before_t is_before_ptr;
-	void *is_before_param;
-};
-
-struct srt_iterator
-{
-	dll_iter_t iterator;
-};
+int PrintSortedList(void *data, void *param);
+int AddToNode(void *data, void *param);
+int FindNumber(const void *data, const void *param);
 
 int main()
 {
 	TestCreateDestroy();
 	TestInsert();
-
+	TestPopFrontSize();
+	TestPopBackSize();	
+	TestRemove();
+	TestMerge();
+	TestForEach();
+/*	TestFindIf();
+*/	
 	return 0;
 }
 
@@ -34,61 +38,401 @@ void TestCreateDestroy()
 	printf("Create srt_list\n");
 	printf("Destroy srt_list\n");
 	SrtListDestroy(new_srtlist);
+	printf("Check valgrind for non leeks\n\n");
 }
 
 void TestInsert()
 {
-	int n1 = 1;
-	int n2 = 2;
-	int n3 = 17;
-	int n4 = 6;
-	int n5 = 22;
-	dll_node_t *node = NULL;
+	int list_members[] = {1,2,17,6,22,6};
+	int list_sorted[] = {1,2,6,6,17,22};
+	srt_iter_t srt_iter = {0};
+	int i = 0;
 	
 	srt_list_t *new_srtlist = SrtListCreate(NULL, IsMatch);
 	printf("Create srt_list\n");
 	
+	for (i = 0; i < 6; ++i)
+	{
+		srt_iter = SrtListInsert(&list_members[i], new_srtlist);
+		PRINTTESTRESULTS("TestInsert_Insert",i ,
+			list_members[i] == *(int *)SrtListGetData(srt_iter));
+	}
 	
-	SrtListInsert(&n1, new_srtlist);
-	for (node = DLListBegin(new_srtlist -> dll);
-		 node != DLListEnd(new_srtlist -> dll);
-		 node = DLListNext(node))
+	i = 0;
+	for (srt_iter = SrtListBegin(new_srtlist);
+		 	 !SrtListIsSameIterator(srt_iter, SrtListEnd(new_srtlist));
+		 	 srt_iter = SrtListNext(srt_iter))
 	{
-		printf("%d, ", *(int *)DLListGetData(node));
-	}	 
-	SrtListInsert(&n2, new_srtlist);
-	for (node = DLListBegin(new_srtlist -> dll);
-		 node != DLListEnd(new_srtlist -> dll);
-		 node = DLListNext(node))
-	{
-		printf("%d, ", *(int *)DLListGetData(node));
-	}	 
-	SrtListInsert(&n3, new_srtlist);
-	for (node = DLListBegin(new_srtlist -> dll);
-		 node != DLListEnd(new_srtlist -> dll);
-		 node = DLListNext(node))
-	{
-		printf("%d, ", *(int *)DLListGetData(node));
-	}	 
-	SrtListInsert(&n4, new_srtlist);
-	for (node = DLListBegin(new_srtlist -> dll);
-		 node != DLListEnd(new_srtlist -> dll);
-		 node = DLListNext(node))
-	{
-		printf("%d, ", *(int *)DLListGetData(node));
-	}	 
-	SrtListInsert(&n5, new_srtlist);
-	for (node = DLListBegin(new_srtlist -> dll);
-		 node != DLListEnd(new_srtlist -> dll);
-		 node = DLListNext(node))
-	{
-		printf("%d, ", *(int *)DLListGetData(node));
-	}	 	
+		PRINTTESTRESULTS("TestInsert_GetData",6 + i, 
+		list_sorted[i] == *(int *)SrtListGetData(srt_iter));
+		++i;
+	}
 	
-	printf("Destroy srt_list\n");
+	i = 0;
+	for (srt_iter = SrtListPrev(SrtListEnd(new_srtlist));
+		 !SrtListIsSameIterator(srt_iter, 
+		 						SrtListPrev(SrtListBegin(new_srtlist)));
+		 srt_iter = SrtListPrev(srt_iter))
+	{
+		PRINTTESTRESULTS("TestInsert_GetData",12 + i, 
+		list_sorted[5 - i] == *(int *)SrtListGetData(srt_iter));
+		++i;
+	} 	 
+				   
+	printf("Destroy srt_list\n\n");
 	SrtListDestroy(new_srtlist);
 }
 
+void TestPopFrontSize()
+{
+	int list_members[] = {1,2,17,6};
+	int list_sorted1[] = {1,2,6,17};
+	int list_sorted2[] = {2,6,17};
+	int list_sorted3[] = {6,17};
+	int list_sorted4[] = {17};
+	srt_iter_t srt_iter = {0};
+	int i = 0;
+	
+	srt_list_t *new_srtlist = SrtListCreate(NULL, IsMatch);
+	printf("Create srt_list\n");
+	
+	for (i = 0; i < 4; ++i)
+	{
+		srt_iter = SrtListInsert(&list_members[i], new_srtlist);
+		PRINTTESTRESULTS("TestPopFrontSize_Insert",i ,
+			list_members[i] == *(int *)SrtListGetData(srt_iter));
+	}
+	PRINTTESTRESULTS("TestPopFrontSize_Empty",i , 
+					 0 == SrtListIsEmpty(new_srtlist));
+	PRINTTESTRESULTS("TestPopFrontSize_Size",i , 4 == SrtListSize(new_srtlist));
+	
+	PRINTTESTRESULTS("TestPopFrontSize_PopFront",i ,
+			list_sorted1[0] == *(int *)SrtListPopFront(new_srtlist));
+	PRINTTESTRESULTS("TestPopFrontSize_Empty",i , 
+					 0 == SrtListIsEmpty(new_srtlist));		
+	PRINTTESTRESULTS("TestPopFrontSize_Size",i , 3 == SrtListSize(new_srtlist));		
+	i = 0;
+	for (srt_iter = SrtListBegin(new_srtlist);
+		 	 !SrtListIsSameIterator(srt_iter, SrtListEnd(new_srtlist));
+		 	 srt_iter = SrtListNext(srt_iter))
+	{
+		PRINTTESTRESULTS("TestPopFrontSize_GetData",4 + i, 
+		list_sorted2[i] == *(int *)SrtListGetData(srt_iter));
+		++i;
+	}
+	
+	PRINTTESTRESULTS("TestPopFrontSize_PopFront",8 + i,
+			list_sorted1[1] == *(int *)SrtListPopFront(new_srtlist));
+	PRINTTESTRESULTS("TestPopFrontSize_Empty",i , 
+					 0 == SrtListIsEmpty(new_srtlist));		
+	PRINTTESTRESULTS("TestPopFrontSize_Size",i , 2 == SrtListSize(new_srtlist));		
+	i = 0;
+	for (srt_iter = SrtListBegin(new_srtlist);
+		 	 !SrtListIsSameIterator(srt_iter, SrtListEnd(new_srtlist));
+		 	 srt_iter = SrtListNext(srt_iter))
+	{
+		PRINTTESTRESULTS("TestPopFrontSize_GetData",4 + i, 
+		list_sorted3[i] == *(int *)SrtListGetData(srt_iter));
+		++i;
+	}
+	
+	PRINTTESTRESULTS("TestPopFrontSize_PopFront",8 + i,
+			list_sorted1[2] == *(int *)SrtListPopFront(new_srtlist));
+	PRINTTESTRESULTS("TestPopFrontSize_Empty",i , 
+					 0 == SrtListIsEmpty(new_srtlist));		
+	PRINTTESTRESULTS("TestPopFrontSize_Size",i , 1 == SrtListSize(new_srtlist));		
+	i = 0;
+	for (srt_iter = SrtListBegin(new_srtlist);
+		 	 !SrtListIsSameIterator(srt_iter, SrtListEnd(new_srtlist));
+		 	 srt_iter = SrtListNext(srt_iter))
+	{
+		PRINTTESTRESULTS("TestPopFrontSize_GetData",4 + i, 
+		list_sorted4[i] == *(int *)SrtListGetData(srt_iter));
+		++i;
+	}
+	
+	PRINTTESTRESULTS("TestPopFrontSize_PopFront",8 + i,
+			list_sorted1[3] == *(int *)SrtListPopFront(new_srtlist));
+	PRINTTESTRESULTS("TestPopFrontSize_Empty",i , 
+					 1 == SrtListIsEmpty(new_srtlist));		
+	PRINTTESTRESULTS("TestPopFrontSize_Size",i , 0 == SrtListSize(new_srtlist));		
+	
+	printf("Destroy srt_list\n\n");
+	SrtListDestroy(new_srtlist);
+}
+
+void TestPopBackSize()
+{
+	int list_members[] = {1,2,17,6};
+	int list_sorted1[] = {1,2,6,17};
+	int list_sorted2[] = {1,2,6};
+	int list_sorted3[] = {1,2};
+	int list_sorted4[] = {1};
+	srt_iter_t srt_iter = {0};
+	int i = 0;
+	
+	srt_list_t *new_srtlist = SrtListCreate(NULL, IsMatch);
+	printf("Create srt_list\n");
+	
+	for (i = 0; i < 4; ++i)
+	{
+		srt_iter = SrtListInsert(&list_members[i], new_srtlist);
+		PRINTTESTRESULTS("TestPopBackSize_Insert",i ,
+			list_members[i] == *(int *)SrtListGetData(srt_iter));
+	}
+	
+	PRINTTESTRESULTS("TestPopBackSize_Size",i , 4 == SrtListSize(new_srtlist));
+	
+	PRINTTESTRESULTS("TestPopBackSize_PopBack",i ,
+			list_sorted1[3] == *(int *)SrtListPopBack(new_srtlist));
+	PRINTTESTRESULTS("TestPopBackSize_Size",i , 3 == SrtListSize(new_srtlist));		
+	i = 0;
+	for (srt_iter = SrtListBegin(new_srtlist);
+		 	 !SrtListIsSameIterator(srt_iter, SrtListEnd(new_srtlist));
+		 	 srt_iter = SrtListNext(srt_iter))
+	{
+		PRINTTESTRESULTS("TestPopBackSize_GetData",4 + i, 
+		list_sorted2[i] == *(int *)SrtListGetData(srt_iter));
+		++i;
+	}
+	
+	PRINTTESTRESULTS("TestPopBackSize_PopBack",8 + i,
+			list_sorted2[2] == *(int *)SrtListPopBack(new_srtlist));
+	PRINTTESTRESULTS("TestPopBackSize_Size",i , 2 == SrtListSize(new_srtlist));		
+	i = 0;
+	for (srt_iter = SrtListBegin(new_srtlist);
+		 	 !SrtListIsSameIterator(srt_iter, SrtListEnd(new_srtlist));
+		 	 srt_iter = SrtListNext(srt_iter))
+	{
+		PRINTTESTRESULTS("TestPopBackSize_GetData",4 + i, 
+		list_sorted3[i] == *(int *)SrtListGetData(srt_iter));
+		++i;
+	}
+	
+	PRINTTESTRESULTS("TestPopBackSize_PopBack",8 + i,
+			list_sorted3[1] == *(int *)SrtListPopBack(new_srtlist));
+	PRINTTESTRESULTS("TestPopBackSize_Size",i , 1 == SrtListSize(new_srtlist));		
+	i = 0;
+	for (srt_iter = SrtListBegin(new_srtlist);
+		 	 !SrtListIsSameIterator(srt_iter, SrtListEnd(new_srtlist));
+		 	 srt_iter = SrtListNext(srt_iter))
+	{
+		PRINTTESTRESULTS("TestPopBackSize_GetData",4 + i, 
+		list_sorted4[i] == *(int *)SrtListGetData(srt_iter));
+		++i;
+	}
+	
+	PRINTTESTRESULTS("TestPopBackSize_PopBack",8 + i,
+			list_sorted4[0] == *(int *)SrtListPopBack(new_srtlist));
+	PRINTTESTRESULTS("TestPopBackSize_Size",i , 0 == SrtListSize(new_srtlist));		
+	
+	printf("Destroy srt_list\n\n");
+	SrtListDestroy(new_srtlist);
+}
+
+void TestRemove()
+{
+	int list_members[] = {1,2,17,6,2,9};
+	int list_sorted2[] = {2,2,6,9,17};
+	int list_sorted3[] = {2,6,9,17};
+	int list_sorted4[] = {2,6,9};
+	srt_iter_t srt_iter = {0};
+	srt_iter_t removed_srt_iter = {0};
+	
+	int i = 0;
+	
+	srt_list_t *new_srtlist = SrtListCreate(NULL, IsMatch);
+	printf("Create srt_list\n");
+	
+	for (i = 0; i < 6; ++i)
+	{
+		srt_iter = SrtListInsert(&list_members[i], new_srtlist);
+		PRINTTESTRESULTS("TestRemove_Insert",i ,
+			list_members[i] == *(int *)SrtListGetData(srt_iter));
+	}
+	
+	PRINTTESTRESULTS("TestRemove_Size",i , 6 == SrtListSize(new_srtlist));
+	
+	removed_srt_iter = SrtListBegin(new_srtlist);
+	srt_iter = SrtListNext(removed_srt_iter);
+	PRINTTESTRESULTS("TestRemove_Remove",i ,
+			SrtListIsSameIterator(srt_iter, SrtListRemove(removed_srt_iter)));
+			
+	PRINTTESTRESULTS("TestRemove_Size",i , 5 == SrtListSize(new_srtlist));	
+	
+	i = 0;
+	for (srt_iter = SrtListBegin(new_srtlist);
+		 	 !SrtListIsSameIterator(srt_iter, SrtListEnd(new_srtlist));
+		 	 srt_iter = SrtListNext(srt_iter))
+	{
+		PRINTTESTRESULTS("TestRemove_GetData",4 + i, 
+		list_sorted2[i] == *(int *)SrtListGetData(srt_iter));
+		++i;
+	}
+	
+	/**/
+	removed_srt_iter = SrtListNext(SrtListBegin(new_srtlist));
+	srt_iter = SrtListNext(removed_srt_iter);
+	PRINTTESTRESULTS("TestRemove_Remove",i ,
+			SrtListIsSameIterator(srt_iter, SrtListRemove(removed_srt_iter)));
+			
+	PRINTTESTRESULTS("TestRemove_Size",i , 4 == SrtListSize(new_srtlist));
+	
+	i = 0;
+	for (srt_iter = SrtListBegin(new_srtlist);
+		 	 !SrtListIsSameIterator(srt_iter, SrtListEnd(new_srtlist));
+		 	 srt_iter = SrtListNext(srt_iter))
+	{
+		PRINTTESTRESULTS("TestRemove_GetData",4 + i, 
+		list_sorted3[i] == *(int *)SrtListGetData(srt_iter));
+		++i;
+	}
+	
+	/**/
+	removed_srt_iter = SrtListPrev(SrtListEnd(new_srtlist));
+	srt_iter = SrtListEnd(new_srtlist);
+	PRINTTESTRESULTS("TestRemove_Remove",i ,
+			SrtListIsSameIterator(srt_iter, SrtListRemove(removed_srt_iter)));
+			
+	PRINTTESTRESULTS("TestRemove_Size",i , 3 == SrtListSize(new_srtlist));
+	
+	i = 0;
+	for (srt_iter = SrtListBegin(new_srtlist);
+		 	 !SrtListIsSameIterator(srt_iter, SrtListEnd(new_srtlist));
+		 	 srt_iter = SrtListNext(srt_iter))
+	{
+		PRINTTESTRESULTS("TestRemove_GetData",4 + i, 
+		list_sorted4[i] == *(int *)SrtListGetData(srt_iter));
+		++i;
+	}
+		
+	
+	printf("Destroy srt_list\n\n");
+	SrtListDestroy(new_srtlist);
+}
+
+void TestMerge()
+{
+	int list_members1[] = {1,2,17,6,2,9,4,8,12,14};
+	int list_members2[] = {23,-89,-65,-8,12,512,-1202,1,5};
+	int list_merged[] = {-1202,-89,-65,-8,1,1,2,2,4,5,6,8,9,12,12,14,17,23,512};
+	srt_iter_t srt_iter = {0};
+	int i = 0;
+	
+	srt_list_t *new_srtlist1 = SrtListCreate(NULL, IsMatch);
+	srt_list_t *new_srtlist2 = SrtListCreate(NULL, IsMatch);
+	printf("Create two srt_list\n");
+	
+	for (i = 0; i < 10; ++i)
+	{
+		srt_iter = SrtListInsert(&list_members1[i], new_srtlist1);
+		PRINTTESTRESULTS("TestMerge_Insert",i ,
+			list_members1[i] == *(int *)SrtListGetData(srt_iter));
+	}
+	PRINTTESTRESULTS("TestMerge_Size",i , 10 == SrtListSize(new_srtlist1));
+	
+	for (i = 0; i < 9; ++i)
+	{
+		srt_iter = SrtListInsert(&list_members2[i], new_srtlist2);
+		PRINTTESTRESULTS("TestMerge_Insert",i ,
+			list_members2[i] == *(int *)SrtListGetData(srt_iter));
+	}
+	PRINTTESTRESULTS("TestMerge_Size",i , 9 == SrtListSize(new_srtlist2));
+	
+	printf("Merge lists\n");
+	SrtListMerge(new_srtlist1, new_srtlist2);
+	
+	i = 0;
+	for (srt_iter = SrtListBegin(new_srtlist2);
+		 	 !SrtListIsSameIterator(srt_iter, SrtListEnd(new_srtlist2));
+		 	 srt_iter = SrtListNext(srt_iter))
+	{
+		PRINTTESTRESULTS("TestRemove_GetData",4 + i, 
+		list_merged[i] == *(int *)SrtListGetData(srt_iter));
+		++i;
+	}
+	
+	printf("Destroy srt_list\n\n");
+	SrtListDestroy(new_srtlist2);
+}
+
+void TestForEach()
+{
+	int list_members[] = {1,2,17,6,2,9};
+	int list_sorted[] = {1,2,2,6,9,17};
+	srt_iter_t srt_iter = {0};	
+	int i = 0;
+	int param = 1;
+	
+	srt_list_t *new_srtlist = SrtListCreate(NULL, IsMatch);
+	printf("Create srt_list\n");
+	
+	for (i = 0; i < 6; ++i)
+	{
+		srt_iter = SrtListInsert(&list_members[i], new_srtlist);
+		PRINTTESTRESULTS("TestForEach_Insert",i ,
+			list_members[i] == *(int *)SrtListGetData(srt_iter));
+	}
+	
+	i = 0;
+	for (srt_iter = SrtListBegin(new_srtlist);
+		 	 !SrtListIsSameIterator(srt_iter, SrtListEnd(new_srtlist));
+		 	 srt_iter = SrtListNext(srt_iter))
+	{
+		PRINTTESTRESULTS("TestRemove_GetData",4 + i, 
+		list_sorted[i] == *(int *)SrtListGetData(srt_iter));
+		++i;
+	}
+	
+	PRINTTESTRESULTS("TestForEach_ForEach",i, 
+	0 == SrtListForEach(SrtListBegin(new_srtlist), 
+	SrtListEnd(new_srtlist), &param, AddToNode));
+	
+	i = 0;
+	for (srt_iter = SrtListBegin(new_srtlist);
+		 	 !SrtListIsSameIterator(srt_iter, SrtListEnd(new_srtlist));
+		 	 srt_iter = SrtListNext(srt_iter))
+	{
+		PRINTTESTRESULTS("TestRemove_GetData",4 + i, 
+		(list_sorted[i] + 1) == *(int *)SrtListGetData(srt_iter));
+		++i;
+	}
+	
+	printf("Destroy srt_list\n\n");
+	SrtListDestroy(new_srtlist);
+}
+/*
+void TestFindIf()
+{
+	int list_members[] = {1,2,17,6,2,9};	
+	int list_sorted[] = {1,2,2,6,9,17};
+	srt_iter_t srt_iter = {0};	
+	int i = 0;
+	int param_to_find = 17;
+	int param_not_to_find = 3;	
+	int FindNumber(const void *data, const void *param);
+	srt_list_t *new_srtlist = SrtListCreate(NULL, IsMatch);
+	printf("Create srt_list\n");
+	
+	for (i = 0; i < 6; ++i)
+	{
+		srt_iter = SrtListInsert(&list_members[i], new_srtlist);
+		PRINTTESTRESULTS("TestFindIf_Insert",i ,
+			list_members[i] == *(int *)SrtListGetData(srt_iter));
+	}
+	
+	PRINTTESTRESULTS("TestFindIf_FindIf",i ,
+			1 == SrtListFindIf(SrtListBegin(new_srtlist), SrtListEnd(new_srtlist), 
+						 &param_to_find, FindNumber));
+						 
+	PRINTTESTRESULTS("TestFindIf_FindIf",i ,
+			0 == SrtListFindIf(SrtListBegin(new_srtlist), SrtListEnd(new_srtlist), 
+						 &param_not_to_find, FindNumber));					 
+	
+	printf("Destroy srt_list\n\n");
+	SrtListDestroy(new_srtlist);
+}
+*/
 /********************************************************/
 /* list is sorted small to big */
 int IsMatch(const void *data1, const void *data2, void *param)
@@ -96,4 +440,25 @@ int IsMatch(const void *data1, const void *data2, void *param)
 	(void)param;
 	
 	return (*(int *)data1 <= *(int *)data2);
+}
+
+/* print data n a node */
+int PrintSortedList(void *data, void *param)
+{
+	printf("%d, ", *(int *)data);
+	(void)param;
+	
+	return 0;
+}
+
+int AddToNode(void *data, void *param)
+{
+	*(int *)data += *(int *)param;
+		
+	return 0;
+}
+
+int FindNumber(const void *data, const void *param)
+{
+	return (*(int *)data == *(int *)param);
 }
