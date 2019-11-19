259acab482715a59680ff568b366b952ed0d914a
tamir.dayan <tamirda@gmail.com>
2019-11-19 08:07:54 +0200

    add a test to PQ

--- ds/pq/PQ_tests.c
+++ ds/pq/PQ_tests.c
@@ -10,7 +10,9 @@ void TestEnqueuePeek();
 void TestDequeue();
 void TestClear();
 void TestErase();
+void TestBetterEnqueuePeek();
 int MyCompareFunc(const void *new_data, const void *src_data, void *param);
+int MyBetterCompareFunc(const void *new_data, const void *src_data, void *param);
 int IsMatch(const void *data1, const void *data2);
 
 int main()
@@ -19,7 +21,8 @@ int main()
 	TestEnqueuePeek();
 	TestDequeue();
 	TestClear();
-	TestErase();		
+	TestErase();
+	TestBetterEnqueuePeek();			
 	
 
 	return 0;
@@ -66,6 +69,43 @@ void TestEnqueuePeek()
 	PRINTTESTRESULTS("TestEnqueuePeek_Enqueue",17, 0 == PQEnqueue(new_pq, &n6));
 	PRINTTESTRESULTS("TestEnqueuePeek_Isempty",18, 0 == PQIsEmpty(new_pq));	
 	PRINTTESTRESULTS("TestEnqueuePeek_Peek",19, 10 == *(int *)PQPeek(new_pq));
+	
+	
+	printf("Destroy priority queue\n\n");
+	PQDestroy(new_pq);
+}
+
+void TestBetterEnqueuePeek()
+{
+	int n1 = 2;
+	int n2 = 12;
+	int n3 = 13;
+	int n4 = 11;
+	int n5 = 27;
+	int n6 = 17;
+	int priority_param = 0;
+	p_queue_t *new_pq = PQCreate(&priority_param, MyBetterCompareFunc);
+	printf("Create priority queue\n");
+	
+	PRINTTESTRESULTS("TestEnqueuePeek_Isempty",1, 1 == PQIsEmpty(new_pq));
+	PRINTTESTRESULTS("TestEnqueuePeek_Enqueue",2, 0 == PQEnqueue(new_pq, &n1));
+	PRINTTESTRESULTS("TestEnqueuePeek_Isempty",3, 0 == PQIsEmpty(new_pq));
+	PRINTTESTRESULTS("TestEnqueuePeek_Peek",4, 2 == *(int *)PQPeek(new_pq));
+	PRINTTESTRESULTS("TestEnqueuePeek_Enqueue",12, 0 == PQEnqueue(new_pq, &n2));
+	PRINTTESTRESULTS("TestEnqueuePeek_Isempty",6, 0 == PQIsEmpty(new_pq));
+	PRINTTESTRESULTS("TestEnqueuePeek_Peek",7, 2 == *(int *)PQPeek(new_pq));
+	PRINTTESTRESULTS("TestEnqueuePeek_Enqueue",13, 0 == PQEnqueue(new_pq, &n3));
+	PRINTTESTRESULTS("TestEnqueuePeek_Isempty",9, 0 == PQIsEmpty(new_pq));
+	PRINTTESTRESULTS("TestEnqueuePeek_Peek",10, 13 == *(int *)PQPeek(new_pq));
+	PRINTTESTRESULTS("TestEnqueuePeek_Enqueue",11, 0 == PQEnqueue(new_pq, &n4));
+	PRINTTESTRESULTS("TestEnqueuePeek_Isempty",12, 0 == PQIsEmpty(new_pq));	
+	PRINTTESTRESULTS("TestEnqueuePeek_Peek",13, 13 == *(int *)PQPeek(new_pq));
+	PRINTTESTRESULTS("TestEnqueuePeek_Enqueue",27, 0 == PQEnqueue(new_pq, &n5));
+	PRINTTESTRESULTS("TestEnqueuePeek_Isempty",15, 0 == PQIsEmpty(new_pq));	
+	PRINTTESTRESULTS("TestEnqueuePeek_Peek",16, 27 == *(int *)PQPeek(new_pq));
+	PRINTTESTRESULTS("TestEnqueuePeek_Enqueue",17, 0 == PQEnqueue(new_pq, &n6));
+	PRINTTESTRESULTS("TestEnqueuePeek_Isempty",18, 0 == PQIsEmpty(new_pq));	
+	PRINTTESTRESULTS("TestEnqueuePeek_Peek",19, 27 == *(int *)PQPeek(new_pq));
 	
 	
 	printf("Destroy priority queue\n\n");
@@ -199,6 +239,24 @@ int MyCompareFunc(const void *new_data, const void *src_data, void *param)
 		return -1;
 	}
 	else if (*(int *)new_data == *(int *)src_data)
+	{
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+}
+
+int MyBetterCompareFunc(const void *new_data, const void *src_data, void *param)
+{
+	(void)param;
+	
+	if ((*(int *)new_data % 10) < (*(int *)src_data % 10))
+	{
+		return -1;
+	}
+	else if ((*(int *)new_data % 10) == (*(int *)src_data % 10))
 	{
 		return 0;
 	}
