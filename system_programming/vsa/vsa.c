6afa79ac10eec984a98f4ecb48d1177e7d1985b6
chen.rubin <tamirda@gmail.com>
2019-12-04 10:19:08 +0200

    fix after code review

--- system_programming/vsa/vsa.c
+++ system_programming/vsa/vsa.c
@@ -7,12 +7,14 @@
 ************************************/
 #include <stdlib.h> /* NULL */
 #include <assert.h> /* assert */
+#include <stdio.h> /* assert */
 
 #include "vsa.h"
 #include "MyUtils.h" /* MAX2,MIN2 */
 
 #define MAGIC 0x12345678
 #define WORDSIZE sizeof(size_t)
+#define HEADER_SIZE sizeof(vsa_t)
 
 typedef struct block_header
 {
@@ -32,14 +34,15 @@ static vsa_t *FindFirstFreeSpaceIMP(vsa_t *vsa);
 
 /* determines if there is enough free space to allocate for size satrting vsa
    is_size_relevant is used in Largest chunk when I look for largest */
-static size_t FreeSpaceToUse(vsa_t *vsa, size_t size, int is_size_relevant);
+static size_t FreeSpaceToUseIMP(vsa_t *vsa, size_t size, int is_size_relevant);
 
 /* advances header in step */
-static vsa_t *AdvanceHeader(vsa_t *header, size_t step);
+static vsa_t *AdvanceHeaderIMP(vsa_t *header, size_t step);
+ssize_t MaxSize(ssize_t a, ssize_t b);
 
 vsa_t *VSAInit(void *memory_pool, size_t pool_size)
 {
-	size_t actual_size = pool_size - 2*sizeof(vsa_t);
+	size_t actual_size = pool_size - 2*HEADER_SIZE;
 	vsa_block_header_t *header = NULL;	
 	
 	assert(memory_pool);
@@ -54,10 +57,10 @@ vsa_t *VSAInit(void *memory_pool, size_t pool_size)
 	#ifdef NDEBUG
 	((vsa_t *)((char *)memory_pool + 
 					   pool_size - 
-					   sizeof(vsa_t))) -> magic_number = MAGIC;
+					   HEADER_SIZE)) -> magic_number = MAGIC;
 	#endif
 	
-	header = AdvanceHeader(header, pool_size - sizeof(vsa_t));
+	header = AdvanceHeaderIMP(header, pool_size - HEADER_SIZE);
 	header -> block_size = 0;
 	
 	return (vsa_t *)memory_pool;
@@ -75,7 +78,7 @@ void *VSAAlloc(vsa_t *vsa, size_t block_size)
 	assert(vsa);
 	
 	first_header = vsa;
-	addition = sizeof(vsa_t) + size_after_padding;
+	addition = HEADER_SIZE + size_after_padding;
 	
 	if (0 == block_size)
 	{
@@ -85,9 +88,9 @@ void *VSAAlloc(vsa_t *vsa, size_t block_size)
 	while (first_header -> block_size)
 	{
 		first_header = FindFirstFreeSpaceIMP(first_header);
-		space_to_allocate = FreeSpaceToUse(first_header, block_size, 1);
+		space_to_allocate = FreeSpaceToUseIMP(first_header, block_size, 1);
 		
-		if ((size_after_padding + sizeof(vsa_t) + WORDSIZE) <= 
+		if ((size_after_padding + HEADER_SIZE + WORDSIZE) <= 
 										 space_to_allocate)
 		{
 			header_after = (vsa_t *)((char *)first_header + addition);
@@ -98,7 +101,7 @@ void *VSAAlloc(vsa_t *vsa, size_t block_size)
 			first_header -> magic_number = MAGIC;
 			#endif
 			
-			return ((char *)first_header + sizeof(vsa_t));
+			return ((char *)first_header + HEADER_SIZE);
 		}
 		else if (size_after_padding <= space_to_allocate)
 		{
@@ -107,11 +110,11 @@ void *VSAAlloc(vsa_t *vsa, size_t block_size)
 			first_header -> magic_number = MAGIC;
 			#endif
 			
-			return (void *)((char *)first_header + sizeof(vsa_t));
+			return (void *)((char *)first_header + HEADER_SIZE);
 		}
 		
-		first_header = AdvanceHeader(first_header, 
-										 sizeof(vsa_t) + space_to_allocate);
+		first_header = AdvanceHeaderIMP(first_header, 
+										 HEADER_SIZE + space_to_allocate);
 	}
 	
 	return NULL;
@@ -121,17 +124,19 @@ void VSAFree(void *allocated_address)
 {
 	if (allocated_address)
 	{
-		allocated_address = (char *)allocated_address - sizeof(vsa_t);
+		allocated_address = (char *)allocated_address - HEADER_SIZE;
 		
 		#ifdef NDEBUG
 		if (MAGIC == (((vsa_t *)allocated_address) -> magic_number))
 		{
-			((vsa_t *)allocated_address) -> block_size *= (-1);
+			((vsa_t *)allocated_address) -> block_size = 
+				-abs(((vsa_t *)allocated_address) -> block_size);
 		}
 		#endif
 		
 		#ifndef NDEBUG
-		((vsa_t *)allocated_address) -> block_size *= (-1);
+		((vsa_t *)allocated_address) -> block_size = 
+			-abs(((vsa_t *)allocated_address) -> block_size);
 		#endif
 	}	
 }
@@ -149,9 +154,9 @@ size_t VSALargestChunk(vsa_t *vsa)
 	while (0 != runner -> block_size)
 	{
 		runner = FindFirstFreeSpaceIMP(runner);
-		space_to_allocate = FreeSpaceToUse(runner, 0, 0);
+		space_to_allocate = FreeSpaceToUseIMP(runner, 0, 0);
 		max_space = MAX2(max_space, space_to_allocate);
-		runner = AdvanceHeader(runner, space_to_allocate + sizeof(vsa_t));
+		runner = AdvanceHeaderIMP(runner, space_to_allocate + HEADER_SIZE);
 	}
 	
 	return max_space;	
@@ -178,7 +183,7 @@ static vsa_t *FindFirstFreeSpaceIMP(vsa_t *vsa)
 	{		
 		while (vsa -> block_size > 0)
 		{
-			vsa = AdvanceHeader(vsa, (vsa -> block_size) + sizeof(vsa_t));
+			vsa = AdvanceHeaderIMP(vsa, (vsa -> block_size) + HEADER_SIZE);
 		}
 		
 		return vsa;
@@ -187,7 +192,7 @@ static vsa_t *FindFirstFreeSpaceIMP(vsa_t *vsa)
 	return NULL;
 }
 
-static size_t FreeSpaceToUse(vsa_t *vsa, size_t size, int is_size_relevant)
+static size_t FreeSpaceToUseIMP(vsa_t *vsa, size_t size, int is_size_relevant)
 {
 	size_t free_space = 0;
 	ssize_t num_of_headers = -1;
@@ -195,25 +200,29 @@ static size_t FreeSpaceToUse(vsa_t *vsa, size_t size, int is_size_relevant)
 	while ((vsa -> block_size) < 0)
 	{
 		++num_of_headers;
-		free_space += 
-		(-1) * (vsa -> block_size) + num_of_headers * sizeof(vsa_t);
-		vsa = AdvanceHeader(vsa, (-1) * (vsa -> block_size) + sizeof(vsa_t));
+		free_space += (-1) * (vsa -> block_size);
+		vsa = AdvanceHeaderIMP(vsa, (-1) * (vsa -> block_size) + HEADER_SIZE);
 		
 		if (free_space >= size && is_size_relevant)
 		{
 			break;
 		}				
 	}
-	
-	return free_space;
+/*	MAX2(num_of_headers * (ssize_t)HEADER_SIZE, 0)*/
+	return (free_space + MaxSize(num_of_headers * HEADER_SIZE, 0));
 }
 
-static vsa_t *AdvanceHeader(vsa_t *header, size_t step)
+static vsa_t *AdvanceHeaderIMP(vsa_t *header, size_t step)
 {
-	if (sizeof(vsa_t) != step)
+	if (HEADER_SIZE != step)
 	{
 		header = (vsa_t *)((char *)header + step);
 	}
 	
 	return header;
+}
+
+ssize_t MaxSize(ssize_t a, ssize_t b)
+{
+	return (a >= b ? a : b);
 }
