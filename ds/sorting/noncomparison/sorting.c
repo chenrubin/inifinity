8ca3131318c6ec19872642c44307fa7052f11fec
chen.rubin <tamirda@gmail.com>
2019-12-11 18:04:26 +0200

    update sorting before code review

--- ds/sorting/noncomparison/sorting.c
+++ ds/sorting/noncomparison/sorting.c
@@ -2,20 +2,35 @@
 *		Author: ChenR				  *
 *		Reviewer: Sharon			  *
 *		sort						  *
-*		10/12/2019					  *
+*		11/12/2019					  *
 *									  *
 ************************************/
 
 #include <stdio.h> /* printf */
 #include <assert.h> /* assert */
-#include <stdlib.h> /* assert */
+#include <stdlib.h> /* malloc */
 #include <string.h> /* memcpy */
+#include <math.h> /* abs */
 
 #include "../sorting.h"
 #include "../../../chen/MyUtils.h" /* MAX2,MIN2 */
 
+#define HISTOGRAM_SIZE 19
+#define BASE 10
+#define RADIX_MIN (-9)
 
+/* gets the index of bucket in countingSort according to element */
 static size_t IntGetBucketIMP(const void *element, const void *param);
+
+/* finds number of iterations in which to sort radix until it's finished */
+static int FindNumOfIntertionsIMP(int *arr, size_t arr_size);
+
+/* gets the index of bucket in radixSort according to element */
+static size_t RadixBucketIMP(const void *element, const void *param);
+
+/* find highest ordered numbers regardiles of its sign */
+static int FindHighestOrderedNumberIMP(int *arr, size_t arr_size);
+
 void CountingSortIMP(const void *arr_to_sort,
 					 size_t element_size,
 					 size_t arr_size,
@@ -49,7 +64,82 @@ int CountingSort(const int *arr,
 	free(histogram);				
 					
 	return 0;				
-}				 
+}
+
+int RadixSort(int *arr, size_t arr_size)
+{
+	int i = 0;
+	int num_of_sorting_iterations = FindNumOfIntertionsIMP(arr, arr_size);
+	int *intermediate_arr = NULL;
+	unsigned int *histogram = NULL;
+	
+	intermediate_arr = (int *)malloc(arr_size * (sizeof(int)));
+	if (NULL == intermediate_arr)
+	{
+		return 1;
+	}
+	histogram = (unsigned int *)malloc(HISTOGRAM_SIZE * (sizeof(int)));
+	if (NULL == histogram)
+	{
+		return 1;
+	}
+	
+	for (i = 0; i < num_of_sorting_iterations; ++i)
+	{
+		memset(histogram, 0, HISTOGRAM_SIZE * sizeof(int));
+		CountingSortIMP(arr, sizeof(int), arr_size, histogram, HISTOGRAM_SIZE,
+					 RadixBucketIMP, &i, intermediate_arr);
+		memcpy (arr, intermediate_arr, arr_size * sizeof(int)); 			 
+	}
+	free(intermediate_arr);
+	
+	return 0;
+}
+
+static int FindNumOfIntertionsIMP(int *arr, size_t arr_size)
+{
+	int max = 0;
+	size_t num_of_iterations = 0;
+	
+	max = FindHighestOrderedNumberIMP(arr, arr_size);	
+
+	while (0 != max)
+	{
+		num_of_iterations += 1;
+		max = max / BASE;
+	}
+	
+	return num_of_iterations;
+}
+
+static int FindHighestOrderedNumberIMP(int *arr, size_t arr_size)
+{
+	size_t i = 0;
+	int max = 0;
+	
+	for (i = 0; i < arr_size; ++i)
+	{
+		if (max < abs(arr[i]))
+		{
+			max = abs(arr[i]);
+		}
+	}
+	
+	return max;
+}
+
+static size_t RadixBucketIMP(const void *element, const void *param)
+{
+	int i = 0;
+	int res = *(int *)element;
+	
+	for (i = 0; i < *(int *)param; ++i)
+	{
+		res /= BASE;
+	}
+	
+	return (res % BASE) - RADIX_MIN;
+}
 				 
 void CountingSortIMP(const void *arr_to_sort,
 					 size_t element_size,
--- ds/sorting/noncomparison/sorting_tests.c
+++ ds/sorting/noncomparison/sorting_tests.c
@@ -6,12 +6,14 @@
 #include "../sorting.h"
 #include "../../../chen/MyUtils.h" /* MAX2,MIN2 */
 
-#define ARR_LENGTH (2000)
-#define RAND_RANGE (100)
-#define NUM_OF_ITERATIONS (100000)
+#define ARR_LENGTH (10000)
+#define RAND_RANGE (10000)
+#define NUM_OF_ITERATIONS (10000)
 
 void TestCountingSortVsQSort();
 void TestCountingSortManyTimes();
+void TestRadixSortVsQSort();
+void TestRadixSortManyTimes();
 
 int FindMax(int *arr, size_t size);
 int FindMin(int *arr, size_t size);
@@ -23,7 +25,9 @@ int main()
 {
 	TestCountingSortVsQSort();
 	TestCountingSortManyTimes();
-	
+	TestRadixSortVsQSort();	
+	TestRadixSortManyTimes();
+
 	return 0;
 }
 
@@ -54,7 +58,7 @@ void TestCountingSortVsQSort()
 		
 		CountingSort(orig_array,ARR_LENGTH,min,max, arr_res);
 		qsort(arr_exp, ARR_LENGTH, sizeof(int), QSortCompare);
-		status |= memcmp(arr_res, arr_exp, ARR_LENGTH);
+		status |= memcmp(arr_res, arr_exp, ARR_LENGTH * sizeof(int));
 	}
 	
 	PRINTTESTRESULTS("TestCountingSortVsQSort",1, 0 == status);
@@ -88,6 +92,78 @@ void TestCountingSortManyTimes()
 	}
 	end = clock();
 	printf("TestCountingSortManyTimes\n");
+	printf("time = %f seconds\n", ((double) (end - start)) / CLOCKS_PER_SEC);
+}
+
+void TestRadixSortVsQSort()
+{
+	int i = 0;
+	int j = 0;
+	int radix_array[ARR_LENGTH];
+	int arr_exp[ARR_LENGTH];
+	int temp = 0;
+	int status = 0;
+	srand(time(NULL));
+	
+	for (j = 0; j < NUM_OF_ITERATIONS; ++j)
+	{
+		for (i = 0; i < ARR_LENGTH; ++i)
+		{			
+			temp = rand()%RAND_RANGE - RAND_RANGE/2;
+			radix_array[i] = temp;
+			arr_exp[i] = temp;
+		}
+		
+/*		for (i = 0; i < ARR_LENGTH; ++i)
+		{
+			printf("%d ", radix_array[i]);
+		}
+		printf("original array\n");
+*/		
+		RadixSort(radix_array,ARR_LENGTH);
+/*		for (i = 0; i < ARR_LENGTH; ++i)
+		{
+			printf("%d ", radix_array[i]);
+		}
+		printf("after radix\n");
+*/		qsort(arr_exp, ARR_LENGTH, sizeof(int), QSortCompare);
+/*		for (i = 0; i < ARR_LENGTH; ++i)
+		{
+			printf("%d ", arr_exp[i]);
+		}
+		printf("after qsort\n");
+		printf("memcmp = %d\n", memcmp(radix_array, arr_exp, ARR_LENGTH * sizeof(int)));
+*/		status |= memcmp(radix_array, arr_exp, ARR_LENGTH * sizeof(int));
+	}
+	
+	PRINTTESTRESULTS("TestRadixSortVsQSort",1, 0 == status);
+}
+
+void TestRadixSortManyTimes()
+{
+	int i = 0;
+	int j = 0;
+	int orig_array[ARR_LENGTH] = {0};
+	int temp = 0;
+	clock_t start, end;
+	
+	srand(time(NULL));
+    
+	
+	start = clock();
+	for (j = 0; j < NUM_OF_ITERATIONS; ++j)
+	{
+		for (i = 0; i < ARR_LENGTH; ++i)
+		{
+			temp = rand()%RAND_RANGE - RAND_RANGE/2;
+			orig_array[i] = temp;
+		}
+		
+		
+		RadixSort(orig_array,ARR_LENGTH);
+	}
+	end = clock();
+	printf("TestRadixSortManyTimes\n");
 	printf("time = %f seconds\n", ((double) (end - start)) / CLOCKS_PER_SEC);
 }
 
