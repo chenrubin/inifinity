f73b98cffe27a35ff66466df2248c8a5f06d86b7
chen.rubin <chenrubin123@gmail.com>
2019-12-13 13:23:29 +0200

    update bst

--- ds/bst/bst.c
+++ ds/bst/bst.c
@@ -8,9 +8,12 @@
 
 #include <stdio.h> /* printf */
 #include <stdlib.h> /* malloc */
+#include <assert.h> /* assert */
 
 #include "bst.h"
 #include "MyUtils.h" /* MAX2,MIN2 */
+
+
 
 struct bst_node
 {
@@ -27,6 +30,21 @@ struct bst
 	struct bst_node stub;
 };
 
+/* checks if iterator has a right child */
+static int HasRightChildIMP(bst_iter_t);
+
+/* checks if iterator has a left child */
+static int HasLeftChildIMP(bst_iter_t);
+
+/* checks if child is the right child of parent or not */
+static int IsRightChildIMP(bst_iter_t parent, bst_iter_t child);
+
+/* checks if child is the left child of parent or not */
+static int IsLeftChildIMP(bst_iter_t parent, bst_iter_t child);
+
+/* Creates node */
+static bst_iter_t CreateNodeIMP(void *data, bst_iter_t parent);
+
 bst_t *BSTCreate(comparison_func func, void *param)
 {
 	bst_t *new_bst = (bst_t *)malloc(sizeof(bst_t));
@@ -38,30 +56,296 @@ bst_t *BSTCreate(comparison_func func, void *param)
 	new_bst -> comparison_func = func;
 	new_bst -> comparison_param = param;
 	(new_bst -> stub).data = (void *)0xDEADBEEF;
+	(new_bst -> stub).parent = NULL;
+	(new_bst -> stub).left = NULL;
+	(new_bst -> stub).right = NULL;
 	
 	return new_bst;
 }
 
-void BSTDestroy(bst_t *tree);
+void BSTDestroy(bst_t *tree)
 {
-	bst_iter_t runner = tree -> stub.left;
+	bst_iter_t runner = NULL;
+	int is_right_child = 0;
 	
-	while (NULL != runner)
+	assert(tree);
+	
+	runner = &(tree -> stub);
+	
+	while (NULL != BSTEnd(tree) -> left)
 	{
-		if (DoesHaveRightChild(runner))
+		if (HasRightChildIMP(runner))
 		{
-			if (IsThereAnyChild(runner -> right))
-			{
-				runner = runner -> right;
-			}
-			else
-			{
-					
-			}
+			runner = runner -> right;
 		}
 		else
 		{
-			
+			if (HasLeftChildIMP(runner))
+			{
+				runner = runner -> left;
+			}
+			else
+			{
+				is_right_child = IsRightChildIMP(runner -> parent, runner);
+				runner = runner -> parent;
+				
+				if (is_right_child)
+				{
+					free(runner -> right);
+					runner -> right = NULL;
+				}
+				else
+				{
+					free(runner -> left);
+					runner -> left = NULL;
+				}
+			}
 		}
 	}
+	
+	free(tree);
+}
+
+bst_iter_t BSTInsert(bst_t *tree, void *data)
+{
+	bst_iter_t parent = NULL;
+	bst_iter_t runner = NULL;
+	int child_type_flag = 0;
+	int is_parent_right_child = 0;
+	
+	assert(tree);
+	
+	parent = &(tree -> stub);
+	runner = (tree -> stub.left);
+	
+	while (NULL != runner)
+	{
+		parent = runner;
+		child_type_flag = tree -> comparison_func(data, 
+												  runner -> data, 
+												  tree -> comparison_param);
+		if (child_type_flag < 0)												  
+		{
+			runner = runner -> left;
+			is_parent_right_child = 0;
+		}
+		else
+		{
+			runner = runner -> right;
+			is_parent_right_child = 1;
+		}
+	}
+	
+	if (is_parent_right_child)
+	{
+		parent -> right = CreateNodeIMP(data, parent);
+		
+		return parent -> right;
+	}
+	else
+	{
+		parent -> left = CreateNodeIMP(data, parent);
+		
+		return parent -> left;
+	}
+}
+
+static int IsRightChildIMP(bst_iter_t parent, bst_iter_t child)
+{
+	if ((parent -> right) == child)
+	{
+		return 1;
+	}
+	
+	return 0;
+}
+
+static int IsLeftChildIMP(bst_iter_t parent, bst_iter_t child)
+{
+	if ((parent -> left) == child)
+	{
+		return 1;
+	}
+	
+	return 0;
+}
+
+static int HasRightChildIMP(bst_iter_t iter)
+{
+	return (NULL != (iter -> right));
+}
+
+static int HasLeftChildIMP(bst_iter_t iter)
+{
+	return (NULL != (iter -> left));
+}
+
+static bst_iter_t CreateNodeIMP(void *data, bst_iter_t parent)
+{
+	bst_iter_t new_node_iter = (bst_iter_t)malloc(sizeof(bst_node_t));
+	if (NULL == new_node_iter)
+	{
+		return NULL;
+	}
+	
+	new_node_iter -> data = data;
+	new_node_iter -> parent = parent;
+	new_node_iter -> left = NULL;
+	new_node_iter -> right = NULL;
+	
+	return new_node_iter;
+}
+
+bst_iter_t BSTEnd(const bst_t *tree)
+{
+	assert(tree);
+	
+	return (bst_iter_t)(&(tree -> stub));
+}
+
+void *BSTGetData(bst_iter_t iter)
+{
+	return (iter -> data);
+}
+
+bst_iter_t BSTNext(bst_iter_t iter)
+{
+	if (HasRightChildIMP(iter))
+	{
+		iter = iter -> right;
+		while (HasLeftChildIMP(iter))
+		{
+			iter = (iter -> left);
+		}
+		
+		return iter;
+	}
+	else
+	{
+		while (IsRightChildIMP(iter -> parent, iter))
+		{
+			iter = (iter -> parent);
+		}
+		
+		return (iter -> parent);
+	}
+	
+	return NULL;
+}
+
+bst_iter_t BSTPrev(bst_iter_t iter)
+{
+	if (HasLeftChildIMP(iter))
+	{
+		iter = iter -> left;
+		while (HasRightChildIMP(iter))
+		{
+			iter = (iter -> right);
+		}
+		
+		return iter;
+	}
+	else
+	{
+		while (IsLeftChildIMP(iter -> parent, iter))
+		{
+			iter = (iter -> parent);
+		}
+		
+		return (iter -> parent);
+	}
+	
+	return NULL;
+}
+
+int BSTIsSameIterator(bst_iter_t iter1, bst_iter_t iter2)
+{
+	if (((iter1 -> parent) == (iter2 -> parent)) &&
+		((iter1 -> left) == (iter2 -> left)) &&
+		((iter1 -> right) == (iter2 -> right)) &&
+		(BSTGetData(iter1) == BSTGetData(iter2)))
+	{
+		return 1;
+	}
+	
+	return 0;
+}
+
+bst_iter_t BSTBegin(const bst_t *tree)
+{
+	bst_iter_t runner = NULL;
+	
+	assert(tree);
+	
+	runner = (bst_iter_t)(&(tree -> stub));
+	
+	while (NULL != (runner -> left))
+	{
+		runner = (runner -> left);
+	}
+	
+	return runner;
+}
+
+size_t BSTSize(const bst_t *tree)
+{
+	size_t counter = 0;
+	bst_iter_t runner = NULL;
+	
+	assert(tree);
+	
+	runner = BSTBegin(tree);
+	while (runner != BSTEnd(tree))
+	{
+		runner = BSTNext(runner);
+		++counter;
+	}
+	
+	return counter;
+}
+
+int BSTIsEmpty(const bst_t *tree)
+{
+	assert(tree);
+	
+	return (NULL == (tree -> stub.left));
+}
+
+bst_iter_t BSTFind(bst_t *tree, void *data)
+{
+	bst_iter_t runner = NULL;
+	
+	assert(tree);
+	
+	for (runner = BSTBegin(tree); 
+		 !BSTIsSameIterator(runner, BSTEnd(tree));
+		 runner = BSTNext(runner))
+	{
+		if (0 == (tree -> comparison_func(data, 
+									BSTGetData(runner), 
+									tree -> comparison_param)))
+		{
+			return runner; 
+		}	
+	}
+	
+	return BSTEnd(tree);
+}
+
+int BSTForEach(bst_iter_t begin, bst_iter_t end, action_func func, void *param)
+{
+	bst_iter_t runner = NULL;
+	(void)param;
+
+	for (runner = begin; 
+		 !BSTIsSameIterator(runner, end);
+		 runner = BSTNext(runner))
+	{
+		if (0 != func(runner -> data, param))
+		{
+			return 1;
+		}
+	}
+	
+	return 0;
 }
--- ds/bst/bst_tests.c
+++ ds/bst/bst_tests.c
@@ -2,24 +2,242 @@
 #include <stdlib.h> /* printf */
 
 #include "bst.h"
-#include "MyUtils.h" /* MAX2,MIN2 */
+#include "../../chen/MyUtils.h" /* MAX2,MIN2 */
 
-void something(int **p1);
+int MyComparisonFunc(const void *new_data, 
+					 const void *src_data, 
+					 const void *comp_param);
+int QSortCompare(const void *num1, const void *num2);
+int MyForEachFunc(void *data, void *for_each_param);
+
+void TestCreateDestroy();
+void TestInsertDestroy();
+void TestNextPrev();
+void TestSizeIsEmpty();
+void TestFind();
+void TestForEach();
 
 int main()
 {
-	int *p1 = NULL;
+	TestCreateDestroy();
+	TestInsertDestroy();
+	TestNextPrev();
+	TestSizeIsEmpty();
+	TestFind();
+	TestForEach();
 	
-	something(&p1);
-	printf("p1 = %d\n", *p1);
-
+	
+	
 	return 0;
 }
 
-void something(int **p1)
+void TestCreateDestroy()
 {
-	int *p2 = *p1;
-	p2 = (int *)malloc(sizeof(int));
+	int param = 0;
 	
-	*p2 = 5;
+	bst_t *new_bst = BSTCreate(MyComparisonFunc, &param);
+	printf("Create bst\n");
+	
+	BSTDestroy(new_bst);
+	printf("Destroy bst\n\n");
+}
+
+void TestInsertDestroy()
+{
+	int param = 0;
+	int values[] = {100,150,300,-20,-50,452,465,0,101,102,103};
+	
+	size_t i = 0;
+	
+	bst_t *new_bst = BSTCreate(MyComparisonFunc, &param);
+	
+	for (i = 0; i < sizeof(values)/sizeof(int); ++i)
+	{
+		PRINTTESTRESULTS("TestInsertDestroy_Insert",i, 
+		values[i] == *(int *)BSTGetData(BSTInsert(new_bst, &values[i])));
+	}
+	
+	BSTDestroy(new_bst);
+	printf("Destroy bst\n\n");
+}
+
+void TestNextPrev()
+{
+	int param = 0;
+	int values[] = {100,150,300,-20,-50,452,465,0,101,102,103};
+/*	bst_iter_t iter_array = (bst_iter_t)malloc(sizeof(values)/sizeof(int));*/
+	
+	bst_t *new_bst = BSTCreate(MyComparisonFunc, &param);
+	
+	bst_iter_t iter4 = BSTInsert(new_bst, &values[4]);
+	bst_iter_t iter3 = BSTInsert(new_bst, &values[3]);
+	bst_iter_t iter7 = BSTInsert(new_bst, &values[7]);
+	bst_iter_t iter0 = BSTInsert(new_bst, &values[0]);
+	bst_iter_t iter8 = BSTInsert(new_bst, &values[8]);
+	bst_iter_t iter9 = BSTInsert(new_bst, &values[9]);
+	bst_iter_t iter10 = BSTInsert(new_bst, &values[10]);
+	bst_iter_t iter1 = BSTInsert(new_bst, &values[1]);
+	bst_iter_t iter2 = BSTInsert(new_bst, &values[2]);
+	bst_iter_t iter5 = BSTInsert(new_bst, &values[5]);
+	bst_iter_t iter6 = BSTInsert(new_bst, &values[6]);
+	
+	PRINTTESTRESULTS("TestNextPrev_next",1, 1 == BSTIsSameIterator(BSTNext(iter4), iter3));
+	PRINTTESTRESULTS("TestNextPrev_next",2, 1 == BSTIsSameIterator(BSTNext(iter3), iter7));
+	PRINTTESTRESULTS("TestNextPrev_next",3, 1 == BSTIsSameIterator(BSTNext(iter7), iter0));
+	PRINTTESTRESULTS("TestNextPrev_next",4, 1 == BSTIsSameIterator(BSTNext(iter0), iter8));
+	PRINTTESTRESULTS("TestNextPrev_next",5, 1 == BSTIsSameIterator(BSTNext(iter8), iter9));
+	PRINTTESTRESULTS("TestNextPrev_next",6, 1 == BSTIsSameIterator(BSTNext(iter9), iter10));
+	PRINTTESTRESULTS("TestNextPrev_next",7, 1 == BSTIsSameIterator(BSTNext(iter10), iter1));
+	PRINTTESTRESULTS("TestNextPrev_next",8, 1 == BSTIsSameIterator(BSTNext(iter1), iter2));
+	PRINTTESTRESULTS("TestNextPrev_next",9, 1 == BSTIsSameIterator(BSTNext(iter2), iter5));
+	PRINTTESTRESULTS("TestNextPrev_next",10, 1 == BSTIsSameIterator(BSTNext(iter5), iter6));
+	
+	PRINTTESTRESULTS("TestNextPrev_prev",1, 1 == BSTIsSameIterator(BSTPrev(iter3), iter4));
+	PRINTTESTRESULTS("TestNextPrev_prev",2, 1 == BSTIsSameIterator(BSTPrev(iter7), iter3));
+	PRINTTESTRESULTS("TestNextPrev_prev",3, 1 == BSTIsSameIterator(BSTPrev(iter0), iter7));
+	PRINTTESTRESULTS("TestNextPrev_prev",4, 1 == BSTIsSameIterator(BSTPrev(iter8), iter0));
+	PRINTTESTRESULTS("TestNextPrev_prev",5, 1 == BSTIsSameIterator(BSTPrev(iter9), iter8));
+	PRINTTESTRESULTS("TestNextPrev_prev",6, 1 == BSTIsSameIterator(BSTPrev(iter10), iter9));
+	PRINTTESTRESULTS("TestNextPrev_prev",7, 1 == BSTIsSameIterator(BSTPrev(iter1), iter10));
+	PRINTTESTRESULTS("TestNextPrev_prev",8, 1 == BSTIsSameIterator(BSTPrev(iter2), iter1));
+	PRINTTESTRESULTS("TestNextPrev_prev",9, 1 == BSTIsSameIterator(BSTPrev(iter5), iter2));
+	PRINTTESTRESULTS("TestNextPrev_prev",10, 1 == BSTIsSameIterator(BSTPrev(iter6), iter5));
+
+/*	printf("Insert values into bst\n");*/
+	
+/*	qsort(iter_array, sizeof(values)/sizeof(int), 
+					  sizeof(bst_iter_t), QSortCompare);
+*/	
+	
+	
+	BSTDestroy(new_bst);
+/*	free(iter_array);*/
+
+	printf("\n\n");
+}
+
+void TestSizeIsEmpty()
+{
+	int param = 0;
+	int values[] = {100,150,300,-20,-50,452,465,0,101,102,103};
+	bst_t *new_bst = BSTCreate(MyComparisonFunc, &param);
+	size_t i = 0;
+	
+	for (i = 0; i < sizeof(values) / sizeof(int); ++i)
+	{
+		if (i == 0)
+		{
+			PRINTTESTRESULTS("TestSizeISEmpty_IsEmpty",2 * i, 1 == BSTIsEmpty(new_bst));
+		}
+		else
+		{
+			PRINTTESTRESULTS("TestSizeISEmpty_IsEmpty",2 * i, 0 == BSTIsEmpty(new_bst));
+		}
+		
+		PRINTTESTRESULTS("TestSize_SizeAndThenInsert",2*(i + 1), i == BSTSize(new_bst));
+		BSTInsert(new_bst, &values[i]);
+	}
+	
+	PRINTTESTRESULTS("TestSize_size",i + 1, i == BSTSize(new_bst));
+	
+	BSTDestroy(new_bst);
+	
+	printf("\n\n");
+}
+
+void TestFind()
+{
+	int param = 0;
+	int values[] = {100,150,300,-20,-50,452,465,0,101,102,103};
+	bst_t *new_bst = BSTCreate(MyComparisonFunc, &param);
+	size_t i = 0;
+	int member_not_to_find = 500;
+	
+	printf("Insert members into bst\n");
+	for (i = 0; i < sizeof(values) / sizeof(int); ++i)
+	{
+		BSTInsert(new_bst, &values[i]);
+	}
+	
+	for (i = 0; i < sizeof(values) / sizeof(int); ++i)
+	{
+		PRINTTESTRESULTS("TestFind_Find", i + 1, 
+		values[i] == *(int *)BSTGetData(BSTFind(new_bst, &values[i])));
+	}
+	
+	PRINTTESTRESULTS("TestFind_Find", i + 1, 
+	BSTIsSameIterator(BSTEnd(new_bst),  BSTFind(new_bst, &member_not_to_find)));
+	
+	BSTDestroy(new_bst);
+	
+	printf("\n\n");
+}
+
+void TestForEach()
+{
+	int param = 0;
+	int values[] = {100,150,300,-20,-50,452,465,0,101,102,103};
+	int values_qsort[] = {100,150,300,-20,-50,452,465,0,101,102,103};
+	int values_sorted[] = {-40,-10,10,110,111,112,113,160,310,462,475};
+	bst_t *new_bst = BSTCreate(MyComparisonFunc, &param);
+	size_t i = 0;
+	int for_each_param = 10;
+	bst_iter_t runner = NULL;
+	
+	printf("Insert members into bst\n");
+	for (i = 0; i < sizeof(values) / sizeof(int); ++i)
+	{
+		BSTInsert(new_bst, &values[i]);
+	}
+
+	qsort(values_qsort, sizeof(values_qsort)/sizeof(int),
+		   	   sizeof(int), QSortCompare);
+	
+	PRINTTESTRESULTS("TestForEach_ForEach", 1, 
+	0 == BSTForEach(BSTBegin(new_bst), BSTEnd(new_bst), 
+					MyForEachFunc, &for_each_param));
+	
+	i = 0;					
+	for (runner = BSTBegin(new_bst); 
+		 !BSTIsSameIterator(runner, BSTEnd(new_bst));
+		 runner = BSTNext(runner))
+	{
+		PRINTTESTRESULTS("TestForEach_GetData", i + 1, 
+		values_sorted[i] == *(int *)BSTGetData(runner));
+		++i;
+	}
+	
+	BSTDestroy(new_bst);
+	
+	printf("\n\n");
+}
+
+int MyComparisonFunc(const void *new_data, 
+					 const void *src_data, 
+					 const void *comp_param)
+{
+	(void)comp_param;
+	
+	if (*(int *)new_data > *(int *)src_data)
+	{
+		return 1;
+	}
+	else if (*(int *)src_data > *(int *)new_data)
+	{
+		return -1;
+	}
+	
+	return 0;
+}
+
+int MyForEachFunc(void *data, void *for_each_param)
+{	
+	*(int *)data += *(int *)for_each_param;
+
+	return 0;
+}
+							   
+int QSortCompare(const void *num1, const void *num2)
+{
+	return (*(int *)num1 - *(int *)num2);
 }
