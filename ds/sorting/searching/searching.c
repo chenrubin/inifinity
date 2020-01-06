0c91063f784e472455af2847edfdd1755318ba54
chen.rubin <chenrubin123@gmail.com>
2020-01-06 22:37:28 +0200

    update searching

--- ds/sorting/searching/searching.c
+++ ds/sorting/searching/searching.c
@@ -6,7 +6,12 @@
 *									  *
 ************************************/
 
+#include <math.h> /* sqrt */
+
 #include "searching.h"
+#include "../../../chen/MyUtils.h" /* MAX2,MIN2 */
+
+int *SearchEachElementIMP(const int *arr, size_t size, int val);
 
 int *BSearch(const int *arr, size_t size, int val)
 {
@@ -26,6 +31,44 @@ int *BSearch(const int *arr, size_t size, int val)
 		return (BSearch(arr1, size/2, val));
 	}
 	
-	return (BSearch(arr1 + size / 2, size/2, val));
+	return (BSearch(arr1 + size / 2, size - size/2, val));
 }
 
+int *JSearch(const int *arr, size_t size, int val)
+{
+	size_t size_sqrt = (size_t)sqrt(size);
+	size_t i = size_sqrt;
+	int *res = NULL;
+	
+	for (i = size_sqrt; i < size; i = i + size_sqrt)
+	{
+		if (arr[i] >= val)
+		{
+			res = SearchEachElementIMP(arr + i - size_sqrt, size_sqrt, val);
+			if (NULL != res)
+			{
+				return res;
+			}
+		}	
+	}
+	i = i - size_sqrt;
+	res = SearchEachElementIMP(arr + i, size - i, val);
+	
+	return res;
+}
+
+int *SearchEachElementIMP(const int *arr, size_t size, int val)
+{
+	size_t i = 0;
+	int *arr1 = (int *)arr;
+	
+	for (i =0; i < size; ++i)
+	{
+		if (val == arr1[i])
+		{
+			return (arr1 + i);
+		}
+	}
+	
+	return NULL;
+}
--- ds/sorting/searching/searching_tests.c
+++ ds/sorting/searching/searching_tests.c
@@ -4,10 +4,12 @@
 #include "../../../chen/MyUtils.h" /* MAX2,MIN2 */
 
 void TestBinarySearch();
+void TestJumpSearch();
 
 int main()
 {
 	TestBinarySearch();
+	TestJumpSearch();
 
 	return 0;
 }
@@ -23,19 +25,47 @@ void TestBinarySearch()
 	res1 = BSearch(arr1, 10, 8);
 	PRINTTESTRESULTS("TestBinarySearch", 1, 8 == *res1);
 	res1 = BSearch(arr1, 10, 10);
-	PRINTTESTRESULTS("TestBinarySearch", 1, 10 == *res1);
+	PRINTTESTRESULTS("TestBinarySearch", 2, 10 == *res1);
 	res1 = BSearch(arr1, 10, 1);
-	PRINTTESTRESULTS("TestBinarySearch", 1, 1 == *res1);
+	PRINTTESTRESULTS("TestBinarySearch", 3, 1 == *res1);
 	res1 = BSearch(arr1, 10, 0);
-	PRINTTESTRESULTS("TestBinarySearch", 1, NULL == res1);
+	PRINTTESTRESULTS("TestBinarySearch", 4, NULL == res1);
 	
 	res2 = BSearch(arr2, 11, 8);
-	PRINTTESTRESULTS("TestBinarySearch", 1, 8 == *res2);
+	PRINTTESTRESULTS("TestBinarySearch", 5, 8 == *res2);
 	res2 = BSearch(arr2, 11, 11);
-	PRINTTESTRESULTS("TestBinarySearch", 1, 11 == *res2);
+	PRINTTESTRESULTS("TestBinarySearch", 6, 11 == *res2);
 	res2 = BSearch(arr2, 11, 1);
-	PRINTTESTRESULTS("TestBinarySearch", 1, 1 == *res2);
+	PRINTTESTRESULTS("TestBinarySearch", 7, 1 == *res2);
 	res2 = BSearch(arr2, 11, 0);
-	PRINTTESTRESULTS("TestBinarySearch", 1, NULL == res2);
+	PRINTTESTRESULTS("TestBinarySearch", 8, NULL == res2);
+	
+}
+
+void TestJumpSearch()
+{
+	int arr1[] = {1,2,3,4,5,6,7,8,9,10};
+	int arr2[] = {1,2,3,4,5,6,7,8,9,10,11};
+	int *res1 = NULL;
+	int *res2 = NULL;
+	
+	
+	res1 = JSearch(arr1, 10, 8);
+	PRINTTESTRESULTS("TestJumpSearch", 1, 8 == *res1);
+	res1 = JSearch(arr1, 10, 10);
+	PRINTTESTRESULTS("TestJumpSearch", 2, 10 == *res1);
+	res1 = JSearch(arr1, 10, 1);
+	PRINTTESTRESULTS("TestJumpSearch", 3, 1 == *res1);
+	res1 = JSearch(arr1, 10, 0);
+	PRINTTESTRESULTS("TestJumpSearch", 4, NULL == res1);
+	
+	res2 = JSearch(arr2, 11, 8);
+	PRINTTESTRESULTS("TestJumpSearch", 5, 8 == *res2);
+	res2 = JSearch(arr2, 11, 11);
+	PRINTTESTRESULTS("TestJumpSearch", 6, 11 == *res2);
+	res2 = JSearch(arr2, 11, 1);
+	PRINTTESTRESULTS("TestJumpSearch", 7, 1 == *res2);
+	res2 = JSearch(arr2, 11, 0);
+	PRINTTESTRESULTS("TestJumpSearch", 8, NULL == res2);
 	
 }
