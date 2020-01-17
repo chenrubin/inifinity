52d5481cac8a68077f27c6372b5679bb0008908d
chen.rubin <chenrubin@gmail.com>
2020-01-17 13:36:42 +0200

    update watchdog

--- system_programming/watchdog/watcher.c
+++ system_programming/watchdog/watcher.c
@@ -1,4 +1,4 @@
-#define _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 200112L
 
 #include <stdio.h>
 #include <pthread.h>
@@ -9,10 +9,13 @@
 #include <string.h>
 #include <stdlib.h> 
 #include <unistd.h> 
+#include <time.h>
+#include <errno.h>
 
 #include "watcher.h"
 #include "scheduler.h"
 #include "uid.h"
+#include "MyUtils.h"
 
 typedef enum 
 {
@@ -34,10 +37,20 @@ typedef struct task_struct
 	int dead_time;
 }is_alive_param_t;
 
+typedef struct time_spec 
+{
+	time_t tv_sec;
+	long tv_nsec;
+}sem_time_t;
+
+size_t counter = 0;
+
 static void *ThreadFunctionRoutineIMP(void *vars);
 int IsAliveRoutine(void *action_func_param);
 int SignalSenderRoutine(void *action_func_param);
 int SchedulerStop(void *action_func_param);
+static void ResetCounterHandlerIMP(int sig);
+int ReturnFail(sem_t *thread_status);
 
 int main()
 {
@@ -87,6 +100,9 @@ static void *ThreadFunctionRoutineIMP(void *vars)
 	sem_t *thread_ready = NULL;
 	sem_t *wtchdg_ready = NULL;
 	pid_t wd_pid = {0};
+	pid_t own_pid = getpid();
+	char own_pid_str[10] = {0};
+	char *wd_env = {0};
 	struct sigaction counter_handle = {0};
 	int interval = 0;
 	int dead_time = 0;
@@ -97,11 +113,21 @@ static void *ThreadFunctionRoutineIMP(void *vars)
 	scheduler_t *new_sched = NULL;
 	ilrd_uid_t task_uid = {0};
 	is_alive_param_t live_param = {0};
+	int IsDNR = 0;
+	int counter = 0;
+	struct timespec ts = {0};
+	
+	/*convert pid_t to string*/
+	sprintf(own_pid_str, "%d\n", (int)own_pid);
+	setenv("APP_ENV", own_pid_str, 0);
+	/* may fail*/
 	
 	interval = variables -> interval;
 	dead_time = variables -> dead_time;
 	status = variables -> status;
-	
+	ts.tv_sec = 2 * dead_time;
+	ts.tv_nsec = 0;    
+
 	live_param.scheduler = new_sched;
 	live_param.dead_time = dead_time;
 	
@@ -117,46 +143,87 @@ static void *ThreadFunctionRoutineIMP(void *vars)
 	
 	thread_status = sem_open("thread_status", O_CREAT, 0777, 0);
 	thread_ready = sem_open("thread_ready", O_CREAT, 0777, 0);
-	wtchdg_ready = sem_open("wtchdg_ready", O_CREAT, 0777, 0);
+	wtchdg_ready = sem_open("moshe", O_CREAT, 0777, 0);
 	
 	counter_handle.sa_flags = 0;
-/*	counter_handle.sa_handler = SetOffCounter;*/
+	counter_handle.sa_handler = ResetCounterHandlerIMP;
 	
 	sigaction(SIGUSR1, &counter_handle, NULL);
 	
 	/* scheduler creation */
 	new_sched = SchedCreate();
-	/* check fail */
-	if (UIDIsBad(SchedAdd(new_sched, (time_t)interval, SignalSenderRoutine,&wd_pid)))
+	if (NULL == new_sched)
+	{
+		ReturnFail(thread_status);
+	}
+	if (UIDIsBad(SchedAdd(new_sched, (time_t)interval, SignalSenderRoutine, &wd_pid)))
 	{
 		printf("SignalSenderRoutine faild to be added\n");
+		
+		ReturnFail(thread_status);
 	}
-	if (UIDIsBad(SchedAdd(new_sched, (time_t)interval, IsAliveRoutine,&live_param)))
+	if (UIDIsBad(SchedAdd(new_sched, (time_t)interval, IsAliveRoutine, &live_param)))
 	{
 		printf("IsAliveRoutine faild to be added\n");
+		
+		ReturnFail(thread_status);
 	}
-	if (UIDIsBad(SchedAdd(new_sched, 10, SchedulerStop,new_sched)))
+	if (UIDIsBad(SchedAdd(new_sched, 30, SchedulerStop,new_sched)))
 	{
 		printf("IsAliveRoutine faild to be added\n");
+		
+		ReturnFail(thread_status);
 	}
+	/* end of creation*/
 	
-	wd_pid = fork();
-	if (0 == wd_pid)
+	if (NULL == getenv("WD_ENV"))
 	{
-		printf("inside child before exec\n");
-		if (-1 == execvp("./wd.out", variables -> argv))
+		wd_pid = fork();
+		if (0 == wd_pid)
 		{
-			printf("exec failed\n");
+			printf("inside child before exec\n");
+			if (-1 == execvp("./wd.out", variables -> argv))
+			{
+				printf("exec failed\n");
+			}
+			printf("inside child after exec\n");
 		}
-		printf("inside child after exec\n");
-	}
-	else
+	}	
+	
+	while (!IsDNR)
 	{
-		printf("I am thread\n");
+		if (dead_time == counter)
+		{
+			wd_pid = fork();
+			if (0 == wd_pid)
+			{
+				printf("inside child before exec\n");
+				if (-1 == execvp("./wd.out", variables -> argv))
+				{
+					printf("exec failed\n");
+				}
+				printf("inside child after exec\n");
+			}
+			
+			counter = 0;
+		}
+		
+		sem_post(thread_ready);
+	/*	sem_wait(wtchdg_ready);*/
+		if (-1 == sem_timedwait(wtchdg_ready, &ts))
+		{
+			printf("Oh dear, something went wrong with read()! %s\n", strerror(errno));
+			printf ("fail to timed wait\n");
+			
+			ReturnFail(thread_status);
+		}
+		/*convert string to pid*/
+		wd_env = getenv("WD_ENV");
+		wd_pid = atoi(wd_env);
+		/* may fail*/ 
+		SchedRun(new_sched);
 	}
-	
-	SchedRun(new_sched);
-	
+
 	sem_post(thread_status);
 	
 	free(argv);
@@ -167,6 +234,14 @@ static void *ThreadFunctionRoutineIMP(void *vars)
 int IsAliveRoutine(void *action_func_param)
 {
 	printf("This is IsAlive routine\n");
+	++counter;
+	
+	if (((is_alive_param_t *)action_func_param) -> dead_time == counter)
+	{
+		SchedStop(((is_alive_param_t *)action_func_param) -> scheduler);
+		
+		return 0;
+	}
 	
 	return 1;
 }
@@ -174,6 +249,8 @@ int IsAliveRoutine(void *action_func_param)
 int SignalSenderRoutine(void *action_func_param)
 {
 	printf("This is SignalSender routine\n");
+	
+	kill(*(pid_t *)action_func_param, SIGUSR1);
 	
 	return 1;
 }
@@ -184,4 +261,21 @@ int SchedulerStop(void *action_func_param)
 	printf("Sched stop\n");
 	
 	return 0;
+}
+
+static void ResetCounterHandlerIMP(int sig)
+{
+	UNUSED(sig);
+	
+	counter = 0; 
+}
+
+int ReturnFail(sem_t *thread_status)
+{
+	int retval = 0;
+	
+	sem_post(thread_status);
+	pthread_exit(&retval);
+	
+	return FAIL;
 }
--- system_programming/watchdog/wd.c
+++ system_programming/watchdog/wd.c
@@ -1,8 +1,160 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <fcntl.h>         
+#include <sys/stat.h>     
+#include <semaphore.h>
+#include <signal.h>
+#include <string.h>
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <time.h>
+#include <errno.h>
+
+#include "watcher.h"
+/*#include "scheduler.h"*/
+/*#include "uid.h"*/
+/*#include "MyUtils.h"*/
 
 int main()
 {
+	sem_t *wtchdg_ready = sem_open("moshe", 0);
 	printf("I am watchdog\n");
 	
 	return 0;
 }
+/*
+static void *WD(void *vars)
+{
+	sem_t *thread_status = NULL;
+	sem_t *thread_ready = NULL;
+	sem_t *wtchdg_ready = NULL;
+	pid_t wd_pid = {0};
+	pid_t own_pid = getpid();
+	char own_pid_str[10] = {0};
+	char *wd_env = {0};
+	struct sigaction counter_handle = {0};
+	int interval = 0;
+	int dead_time = 0;
+	char *argv = NULL;
+	int argv_len = 0;
+	status_t *status = NULL;
+	thread_vars *variables = vars;
+	scheduler_t *new_sched = NULL;
+	ilrd_uid_t task_uid = {0};
+	is_alive_param_t live_param = {0};
+	int IsDNR = 0;
+	int counter = 0;
+	struct timespec ts = {0};
+	
+	/*convert pid_t to string*/
+/*	sprintf(own_pid_str, "%d\n", (int)own_pid);
+	setenv("APP_ENV", own_pid_str, 0);
+	/* may fail*/
+/*	
+	interval = variables -> interval;
+	dead_time = variables -> dead_time;
+	status = variables -> status;
+	ts.tv_sec = 2 * dead_time;
+	ts.tv_nsec = 0;    
+
+	live_param.scheduler = new_sched;
+	live_param.dead_time = dead_time;
+	
+	argv_len = strlen(*(variables -> argv));
+	argv = (char *)malloc(argv_len);
+	if (NULL == argv)
+	{
+		*status = FAIL;
+		return NULL;
+	}
+	
+	memcpy(argv, *(variables -> argv), argv_len);
+	
+	thread_status = sem_open("thread_status", O_CREAT, 0777, 0);
+	thread_ready = sem_open("thread_ready", O_CREAT, 0777, 0);
+	wtchdg_ready = sem_open("wtchdg_ready", O_CREAT, 0777, 0);
+	
+	counter_handle.sa_flags = 0;
+	counter_handle.sa_handler = ResetCounterHandlerIMP;
+	
+	sigaction(SIGUSR1, &counter_handle, NULL);
+	
+	/* scheduler creation */
+/*	new_sched = SchedCreate();
+	if (NULL == new_sched)
+	{
+		ReturnFail(thread_status);
+	}
+	if (UIDIsBad(SchedAdd(new_sched, (time_t)interval, SignalSenderRoutine, &wd_pid)))
+	{
+		printf("SignalSenderRoutine faild to be added\n");
+		
+		ReturnFail(thread_status);
+	}
+	if (UIDIsBad(SchedAdd(new_sched, (time_t)interval, IsAliveRoutine, &live_param)))
+	{
+		printf("IsAliveRoutine faild to be added\n");
+		
+		ReturnFail(thread_status);
+	}
+	if (UIDIsBad(SchedAdd(new_sched, 30, SchedulerStop,new_sched)))
+	{
+		printf("IsAliveRoutine faild to be added\n");
+		
+		ReturnFail(thread_status);
+	}
+	/* end of creation*/
+	
+/*	if (NULL == getenv("WD_ENV"))
+	{
+		wd_pid = fork();
+		if (0 == wd_pid)
+		{
+			printf("inside child before exec\n");
+			if (-1 == execvp("./wd.out", variables -> argv))
+			{
+				printf("exec failed\n");
+			}
+			printf("inside child after exec\n");
+		}
+	}	
+	
+	while (!IsDNR)
+	{
+		if (dead_time == counter)
+		{
+			wd_pid = fork();
+			if (0 == wd_pid)
+			{
+				printf("inside child before exec\n");
+				if (-1 == execvp("./wd.out", variables -> argv))
+				{
+					printf("exec failed\n");
+				}
+				printf("inside child after exec\n");
+			}
+			
+			counter = 0;
+		}
+		
+		sem_post(thread_ready);
+		sem_wait(wtchdg_ready);
+	/*	if (-1 == sem_timedwait(wtchdg_ready, &ts))
+		{
+			printf ("fail to timed wait\n");
+			
+			ReturnFail(thread_status);
+		}
+	*/	/*convert string to pid*/
+/*		wd_env = getenv("WD_ENV");
+		wd_pid = atoi(wd_env);
+		/* may fail*/ 
+/*		SchedRun(new_sched);
+	}
+
+	sem_post(thread_status);
+	
+	free(argv);
+	
+	return NULL;
+}*/
