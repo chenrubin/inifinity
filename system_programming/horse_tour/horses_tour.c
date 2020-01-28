5bc1b68616bd56b6dccb440d9f357cc79643fdfd
chen.rubin <chenrubin123@gmail.com>
2020-01-28 19:17:16 +0200

    update horses_tours

--- system_programming/horse_tour/horses_tour.c
+++ system_programming/horse_tour/horses_tour.c
@@ -1,11 +1,24 @@
 #include <stdio.h> /* printf */
 #include <unistd.h> /* ssize */
+#include <time.h>
 
-#define LSB 1
-#define CHESS_SIZE 3
-#define FULL_PATH 9
+#define LSB (1)
+#define CHESS_SIZE (8)
+#define FULL_PATH (63)
+#define NUM_OF_STEPS 8
+#define ALL_BITS_SET 0xFFFFFFFFFFFFFFFF
 
-void RecHorsesTour(size_t location, size_t *result_path, size_t current_index, size_t prev_loc_bitmap);
+struct timespec {
+               time_t tv_sec;        /* seconds */
+               long   tv_nsec;       /* nanoseconds */
+           };
+typedef enum status
+{
+	SUCCESS,
+	FAIL
+}status_t;           
+
+int RecHorsesTour(size_t location, size_t *result_path, size_t current_index, size_t prev_loc_bitmap);
 int HorsesTour(size_t start, size_t *result_path);
 static int IsFirstTimeIMP(size_t location, size_t prev_loc_bitmap);
 static size_t SetLocationIMP(size_t location, size_t prev_loc_bitmap);
@@ -14,11 +27,12 @@ int IsLastInPath(size_t location, size_t prev_loc_bitmap);
 int IsRightStepLegal(ssize_t next_step, size_t location);
 int IsLeftStepLegal(ssize_t next_step, size_t location);
 
-int main()
+int main(int argc, char *argv[])
 {
 	size_t result_path[CHESS_SIZE * CHESS_SIZE] = {0};
+	int start = atoi(argv[1]);
 	
-	HorsesTour(0, result_path);
+	HorsesTour(start, result_path);
 	
 	return 0;
 }
@@ -27,8 +41,9 @@ int HorsesTour(size_t start, size_t *result_path)
 {
 	int i = 0;
 	size_t prev_loc_bitmap = 0;
+	status_t status = SUCCESS;
 	
-	RecHorsesTour(start, result_path, 0, prev_loc_bitmap);
+	status = RecHorsesTour(start, result_path, 0, prev_loc_bitmap);
 	
 	for (i = 0; i < (CHESS_SIZE * CHESS_SIZE); ++i)
 	{
@@ -39,48 +54,89 @@ int HorsesTour(size_t start, size_t *result_path)
 	return 0;
 }
 
-void RecHorsesTour(size_t location, size_t *result_path, size_t current_index, size_t prev_loc_bitmap)
+int RecHorsesTour(size_t location, size_t *result_path, size_t current_index, size_t prev_loc_bitmap)
 {
 	size_t i = 0; 
+	size_t j = 0;
+	struct timespec ts;
+	static size_t counter = 0;
 	
-	if (!IsFirstTimeIMP(location, prev_loc_bitmap))
-	{
-		return;
-	}
+	ts.tv_sec = 0;        /* seconds */
+    ts.tv_nsec = 500000000; 
 	if (IsLastInPath(location, prev_loc_bitmap))
 	{
+/*		printf("inside IsLastInPath(location, prev_loc_bitmap) = %ld, %ld\n", location, prev_loc_bitmap);*/
 		result_path[FULL_PATH] = location;
 		
-		return;
+		return SUCCESS;
+	}
+	if (!IsFirstTimeIMP(location, prev_loc_bitmap))
+	{
+/*		printf("inside I!IsFirstTimeIMP(location, prev_loc_bitmap) = %ld, %ld\n", location, prev_loc_bitmap);*/
+		return FAIL;
 	}
 	
-	prev_loc_bitmap |= (LSB << location);
-	result_path[current_index] = location;
-	SetLocationIMP(location, prev_loc_bitmap);
 	
-	for (i = 0; i < CHESS_SIZE; ++i)
+	prev_loc_bitmap = SetLocationIMP(location, prev_loc_bitmap);
+	result_path[current_index] = location;
+	
+/*	printf("location = %ld, current_index = %ld\n", location, current_index);
+	for (j = 0; j < (CHESS_SIZE * CHESS_SIZE); ++j)
 	{
-		if (((i < (CHESS_SIZE / 2)) && IsLeftStepLegal(PossibleSteps(location, i), location)) ||
-			((i >= (CHESS_SIZE / 2)) && IsRightStepLegal(PossibleSteps(location, i), location)))
+		printf("%ld, ", result_path[j]);
+	}
+	printf("\n");
+*/	for (i = 0; i < NUM_OF_STEPS; ++i)
+	{
+/*		printf("inside for (i = 0; i < CHESS_SIZE; ++i), i = %ld, current_index = %ld\n", i, current_index);
+/*		for (j = 0; j < CHESS_SIZE * CHESS_SIZE; ++j)
 		{
-			RecHorsesTour(PossibleSteps(location, i), result_path, current_index + 1, prev_loc_bitmap);			
+			printf("%ld, ", result_path[j]);
+		}
+		printf("\n");
+*//*		nanosleep(&ts, &ts);*/
+		/*sleep(1);*/
+		if (((i >= (NUM_OF_STEPS / 2)) && IsLeftStepLegal(PossibleSteps(location, i), location)) ||
+			((i < (NUM_OF_STEPS / 2)) && IsRightStepLegal(PossibleSteps(location, i), location)))
+		{
+/*			printf("next step = %ld\n", PossibleSteps(location, i));*/
+/*			for (j = 0; j < CHESS_SIZE * CHESS_SIZE; ++j)
+			{
+				printf("%ld, ", result_path[j]);
+			}
+		printf("\n");
+*/			status_t status = RecHorsesTour(PossibleSteps(location, i), result_path, current_index + 1, prev_loc_bitmap);
+			if (SUCCESS == status)
+			{
+				return SUCCESS;
+			}
 		}
 	}
+/*	++counter;*/
+/*	printf("went out, counter = %ld\n", counter);*/
+	
+	return FAIL;
 }
 
 static int IsFirstTimeIMP(size_t location, size_t prev_loc_bitmap)
 {	
-	return (!((LSB << location) & prev_loc_bitmap));
+	if (0 != (((size_t)LSB << location) & prev_loc_bitmap))
+	{
+/*		printf("location %ld in %ld!!!!!!!!!!!!!!! Already exists !!!!!!!!!!!!!!!!!!\n", location, prev_loc_bitmap);*/
+	}
+	return (!(((size_t)LSB << location) & prev_loc_bitmap));
 }
 
 static size_t SetLocationIMP(size_t location, size_t prev_loc_bitmap)
 {	
-	return ((LSB << location) | prev_loc_bitmap);
+/*	printf("!!!!!!!!!!prev_loc_bitmap = 0x%lX!!!!!!!!!!!!!!!!!!!!!!!1\n", prev_loc_bitmap);*/
+	
+	return (((size_t)LSB << location) | prev_loc_bitmap);
 }
 
 static ssize_t PossibleSteps(size_t location, size_t step_num)
 {
-	ssize_t possible_steps[CHESS_SIZE] = {0};
+	ssize_t possible_steps[NUM_OF_STEPS] = {0};
 	
 	possible_steps[0] = (ssize_t)location - 2 * CHESS_SIZE + 1;
 	possible_steps[1] = (ssize_t)location + 2 - CHESS_SIZE;
@@ -90,24 +146,27 @@ static ssize_t PossibleSteps(size_t location, size_t step_num)
 	possible_steps[5] = (ssize_t)location - 2 + CHESS_SIZE;
 	possible_steps[6] = (ssize_t)location - 2 - CHESS_SIZE;
 	possible_steps[7] = (ssize_t)location - 2 * CHESS_SIZE - 1;
-	
-	return possible_steps[step_num];
+/*	printf("step_num = %ld\n", step_num);
+/*	printf("step = %ld\n", possible_steps[step_num]);
+*/	return possible_steps[step_num];
 }
 
 int IsLastInPath(size_t location, size_t prev_loc_bitmap)
 {
-	return (FULL_PATH == ((LSB << location) | prev_loc_bitmap));
+	return (ALL_BITS_SET == (((size_t)LSB << location) | prev_loc_bitmap));
 }
 
 int IsRightStepLegal(ssize_t next_step, size_t location)
 {
 	if ((next_step < 0) ||
 		(next_step > FULL_PATH) ||
-		((next_step % CHESS_SIZE) < (location / CHESS_SIZE)))
+		((next_step % CHESS_SIZE) < (location % CHESS_SIZE)))
 	{
 		return 0;
 	}
 	
+/*	printf("inside IsRightStepLegal, location = %ld, next_step = %ld\n",location, next_step);
+*/	
 	return 1;
 }
 
@@ -120,5 +179,7 @@ int IsLeftStepLegal(ssize_t next_step, size_t location)
 		return 0;
 	}
 	
+/*	printf("inside IsLeftStepLegal, next_step = %ld\n", next_step);
+*/	
 	return 1;
 }
