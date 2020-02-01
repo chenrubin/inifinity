a3dd75d0b842e1ba425dbb06c2211b78fa39f190
chen.rubin <chenrubin123@gmail.com>
2020-02-01 23:28:09 +0200

    update c++ excercise

---
+++ cpp_fs/Basics/FunctionOverloading/point.h
@@ -0,0 +1,20 @@
+struct Point
+{
+	enum paran_type
+	{
+		ROUND,
+		SQUARE,
+		CURLY,
+		ANGULAR
+	};
+	void AdjustBy(Point p);
+	void AdjustBy(int dx, int dy);
+	double Length();
+	void Print(char par1, char par2);
+	void Print(enum paran_type);
+	
+	double x;
+	double y;
+};
+
+Point Add(Point p1, Point p2);
