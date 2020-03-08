9d5b478f772ad8e2b9a054f161650e39ee30e461
chen.rubin <chenrubin123@gmail.com>
2020-03-08 17:54:03 +0200

    update udp

--- cpp_fs/ws/udp/client.cpp
+++ cpp_fs/ws/udp/client.cpp
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <stdio.h>
+#include <arpa/inet.h>
 
 #define PROTOCOL (0)
 #define BUFF_SIZE (5)
@@ -15,9 +16,10 @@ int main()
     sockaddr_in server_addr;
     char buff[BUFF_SIZE] = "Pong";
     char read_buff[BUFF_SIZE]; 
+    int broadcast = 1;    
 
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_addr.s_addr = inet_addr("10.1.255.255")/*INADDR_BROADCAST*/;
     server_addr.sin_port = htons(PORT);
 
     int sockfd = socket(AF_INET, SOCK_DGRAM , PROTOCOL);
@@ -26,12 +28,20 @@ int main()
         perror("socket");
     }
 
-    socklen_t size = sizeof(server_addr);
-    for (int i = 0; i < 20; ++i)
+    if (-1 == setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast)))
     {
-        sendto(sockfd, buff, BUFF_SIZE, MSG_CONFIRM, (struct sockaddr *)&server_addr, sizeof(server_addr));
-        recvfrom(sockfd, read_buff, BUFF_SIZE, MSG_WAITALL, (struct sockaddr *)&server_addr, &size);
-        std::cout << "buff = " << read_buff << std::endl;
+        perror("setsockopt");
     }
+
+    socklen_t size = sizeof(server_addr);
+
+ //  sleep(10);
+
+    if (-1 == sendto(sockfd, buff, BUFF_SIZE, MSG_CONFIRM, (struct sockaddr *)&server_addr, sizeof(server_addr)))
+    {
+        perror("client sendto");
+    }
+  //  recvfrom(sockfd, read_buff, BUFF_SIZE, MSG_WAITALL, (struct sockaddr *)&server_addr, &size);
+  //  std::cout << "buff = " << read_buff << std::endl;
 }
     
--- cpp_fs/ws/udp/server.cpp
+++ cpp_fs/ws/udp/server.cpp
@@ -6,6 +6,7 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <cstring> /* memset */
+#include <arpa/inet.h>
 
 #define PROTOCOL (0)
 #define Q_LEN (5)
@@ -17,31 +18,45 @@ int main()
     struct sockaddr_in addr, client_addr;
     char buff[5] = "Ping";
     char read_buff[5];
+    int broadcast = 1; 
     
     addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_addr.s_addr = inet_addr("10.1.255.255"); /*INADDR_BROADCAST*/ /*INADDR_ANY*/;
     addr.sin_port = htons(PORT);
 
-    memset(&client_addr, 0, sizeof(client_addr));
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_addr.s_addr = INADDR_BROADCAST /*INADDR_ANY*/;
+    client_addr.sin_port = htons(PORT);
+   // memset(&client_addr, 0, sizeof(client_addr));
 
-    int sockfd = socket(AF_INET, SOCK_DGRAM , PROTOCOL);
+    int sockfd = socket(AF_INET, SOCK_DGRAM , IPPROTO_UDP);
     if (-1 == sockfd)
     {
         perror("socket");
     }
     
+    if (-1 == setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast)))
+    {
+        perror("setsockopt");
+    }
+
     if(-1 == bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)))
     {
         perror("bind");
     }
 
-    socklen_t size = sizeof(client_addr);
-    for (int i = 0; i < 20; ++i)
+ /*   for (int i = 0; i < 3 ; ++i)
     {
-        recvfrom(sockfd, read_buff, BUFF_SIZE, MSG_WAITALL, (struct sockaddr *)&client_addr, &size);
-        std::cout << "buff = " << read_buff << std::endl;
-        sendto(sockfd, buff, BUFF_SIZE, MSG_CONFIRM, (struct sockaddr *)&client_addr, sizeof(client_addr));
-    }
+*/      socklen_t size = sizeof(client_addr);
+
+//      recvfrom(sockfd, read_buff, BUFF_SIZE, MSG_WAITALL, (struct sockaddr *)&client_addr, &size);
+      std::cout << "buff = " << read_buff << std::endl;
+      if (-1 == sendto(sockfd, buff, BUFF_SIZE, MSG_CONFIRM, (struct sockaddr *)&client_addr, sizeof(client_addr)))
+      {
+          perror("server sendto");
+      }
+  /*  }
+    */
 
     close(sockfd);
 }
