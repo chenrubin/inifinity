17711a3fd7e30569af894c24e1cfffb7fffde6ca
chen.rubin <chenrubin123@gmail.com>
2020-03-29 08:58:03 +0300

    update waitable_queue

--- projects/waitable_queue/waitable_queue.hpp
+++ projects/waitable_queue/waitable_queue.hpp
@@ -5,10 +5,13 @@
 #ifndef __WAITABLE_QUEUE_HPP__ 
 #define __WAITABLE_QUEUE_HPP__
 /*----------------------------------------------------------------------------*/
-#include <boost/thread/mutex.hpp> 	// boost::mutex
 #include <boost/thread/thread.hpp> 	// boost::condition_variable
+#include <boost/thread/mutex.hpp> 	// boost::mutex
+#include <iostream> 	// boost::mutex
 
-#include "MyUtils.hpp"			// class Uncopyable
+#include <queue>    				// queue, priority_queue
+
+#include "MyUtils.hpp"				// class Uncopyable
 
 /*----------------------------------------------------------------------------*/
 namespace ilrd
@@ -36,7 +39,10 @@ public:
 private:
 	boost::mutex m_mutex;
 	boost::condition_variable m_cond;
-	Q m_queue; 
+	Q m_queue;
+
+	typename Q::value_type PeekFuncIMP(std::queue<typename Q::value_type> &queue);
+	typename Q::value_type PeekFuncIMP(std::priority_queue<typename Q::value_type> &queue);
 };
 
 template <typename Q>
@@ -50,7 +56,62 @@ WaitableQueue<Q>::~WaitableQueue()
 template <typename Q>
 void WaitableQueue<Q>::Push(typename Q::const_reference element_)
 {
+	m_mutex.lock();
+	m_queue.push(element_);
+	m_mutex.unlock();
+	m_cond.notify_all();
+}
+
+template <typename Q>
+void WaitableQueue<Q>::Pop(typename Q::reference element_)
+{
+	boost::mutex::scoped_lock lock(m_mutex);
+	if (IsEmpty())
+	{
+		m_cond.wait(lock);
+	}
 	
+	element_ = PeekFuncIMP(m_queue);
+	m_queue.pop();
+}
+
+template <typename Q>
+bool WaitableQueue<Q>::Pop(typename Q::reference element_, 
+						   boost::chrono::milliseconds timeout_)
+{
+	boost::mutex::scoped_lock lock(m_mutex);
+	boost::cv_status status = boost::cv_status::no_timeout;
+
+	if (IsEmpty())
+	{
+		status =  m_cond.wait_for(lock, timeout_);
+	}
+
+	if (status == boost::cv_status::no_timeout)
+	{
+		element_ = PeekFuncIMP(m_queue);
+		m_queue.pop();
+	}
+
+	return (status == boost::cv_status::timeout);
+}
+
+template <typename Q>
+bool WaitableQueue<Q>::IsEmpty() const
+{
+	return m_queue.empty();
+}
+
+template <typename Q>
+typename Q::value_type WaitableQueue<Q>::PeekFuncIMP(std::queue<typename Q::value_type> &queue)
+{
+	return queue.front();
+}
+
+template <typename Q>
+typename Q::value_type WaitableQueue<Q>::PeekFuncIMP(std::priority_queue<typename Q::value_type> &queue)
+{
+	return queue.top();
 }
 
 /*----------------------------------------------------------------------------*/
--- projects/waitable_queue/waitable_queue_tests.cpp
+++ projects/waitable_queue/waitable_queue_tests.cpp
@@ -1,22 +1,252 @@
 #include <queue>    // queue, priority_queue
+#include <pthread.h> /* pthread */
+#include <boost/chrono.hpp>
 
 #include "waitable_queue.hpp"
+#include "MyUtils.hpp"
+
+#define ARGS ((ThreadArgs *)param)
+#define ARGS_SUPER ((ThreadArgsSuper *)param)
+#define SUPER_NUM_OF_POP_THREADS 4
+#define SUPER_NUM_OF_PUSH_THREADS 2
+#define SUPER_POP_ITERATIONS 5
+#define SUPER_NUM_OF_ELEMENTS 10
 
 using namespace ilrd;
-/*
-class MyQueue
-{
-public:
-    MyQueue();
-    ~MyQueue();
 
-private:
-    std::queue<int> myqueue;
+struct ThreadArgs
+{
+    WaitableQueue<std::queue<int> > *queue;
+    int element;
+    boost::chrono::milliseconds timeout;
 };
-*/
+
+struct ThreadArgsSuper
+{
+    WaitableQueue<std::queue<int> > *queue;
+    int *element_arr;
+    size_t arr_size;
+};
+
+void TestPushPop();
+void TestPushPopWTimer();
+void TestPushPopSuper();
+
+void *ThreadPushRoutineIMP(void *param);
+void *ThreadPopRoutineIMP(void *param);
+void *ThreadPopWTimerRoutineIMP(void *param);
+void *ThreadPopRoutineSuperIMP(void *param);
+void *ThreadPushRoutineSuperIMP(void *param);
+
 int main()
 {
-    WaitableQueue<std::queue<int> > myWaitQueue;
-    
+    TestPushPop();
+    TestPushPopWTimer();
+    TestPushPopSuper(); 
+
     return 0;
+}
+
+void TestPushPop()
+{
+    boost::chrono::milliseconds timeout(500);
+    size_t i = 0;
+    const size_t num_of_elements = 14;
+    WaitableQueue<std::queue<int> > *myWaitQueue = new WaitableQueue<std::queue<int> >;
+    int num_arr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+    ThreadArgs thread_args[num_of_elements];
+    
+    for (i = 0; i < num_of_elements; ++i)
+    {
+        thread_args[i].queue = myWaitQueue;
+        thread_args[i].element = num_arr[i];
+        thread_args[i].timeout = timeout;
+    }
+
+    pthread_t thread_arr[num_of_elements];
+
+    for (i = 0; i < num_of_elements; ++i)
+    {
+        myWaitQueue->Push(num_arr[i]);
+    }
+
+    PRINTTESTRESULTS("TestPushPop - Push",1, false == myWaitQueue->IsEmpty());
+
+    for (i = 0; i < num_of_elements; ++i)
+    {
+        pthread_create(&thread_arr[i], NULL, ThreadPopRoutineIMP, &thread_args[i]);
+    }
+
+    for (i = 0; i < num_of_elements; ++i)
+    {
+        int res;
+        void *ret_val = &res;
+        pthread_join(thread_arr[i], &ret_val);
+        std::cout << *static_cast<int *>(ret_val) << ", ";
+    }
+    std::cout << std::endl;
+
+    PRINTTESTRESULTS("TestPushPop - Pop",2, true == myWaitQueue->IsEmpty());
+
+    delete myWaitQueue;
+
+    std::cout << "\n\n";
+}
+
+void TestPushPopWTimer()
+{
+    boost::chrono::milliseconds timeout(3000);
+    size_t i = 0;
+    const size_t num_of_elements = 14;
+    WaitableQueue<std::queue<int> > *myWaitQueue = new WaitableQueue<std::queue<int> >;
+    int num_arr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+    ThreadArgs thread_args[num_of_elements];
+    
+    for (i = 0; i < num_of_elements; ++i)
+    {
+        thread_args[i].queue = myWaitQueue;
+        thread_args[i].element = num_arr[i];
+        thread_args[i].timeout = timeout;
+    }
+
+    pthread_t thread_arr[num_of_elements];
+
+    for (i = 0; i < num_of_elements - 1; ++i)
+    {
+        myWaitQueue->Push(num_arr[i]);
+    }
+
+    PRINTTESTRESULTS("TestPushPopWTimer - Push",1, false == myWaitQueue->IsEmpty());
+
+    for (i = 0; i < num_of_elements; ++i)
+    {
+        pthread_create(&thread_arr[i], NULL, ThreadPopWTimerRoutineIMP, &thread_args[i]);
+    }
+
+    for (i = 0; i < num_of_elements; ++i)
+    {
+        int res;
+        void *ret_val = &res;
+        pthread_join(thread_arr[i], &ret_val);
+        std::cout << *static_cast<int *>(ret_val) << ", ";
+    }
+    std::cout << std::endl;
+
+    PRINTTESTRESULTS("TestPushPopWTimer - Pop",2, true == myWaitQueue->IsEmpty());
+
+    delete myWaitQueue;
+
+    std::cout << "\n\n";
+}
+
+void TestPushPopSuper()
+{
+    boost::chrono::milliseconds timeout(500);
+    size_t i = 0;
+    size_t j = 0;
+    WaitableQueue<std::queue<int> > *myWaitQueue = new WaitableQueue<std::queue<int> >;
+    int num_arr[SUPER_NUM_OF_PUSH_THREADS][SUPER_NUM_OF_ELEMENTS];
+    for (i = 0; i < SUPER_NUM_OF_PUSH_THREADS; ++i)
+    {
+        for (j = 0; j < SUPER_NUM_OF_ELEMENTS; ++j)
+        {
+            num_arr[i][j] = i * SUPER_NUM_OF_PUSH_THREADS + j;
+        }
+    }
+    
+    ThreadArgsSuper thread_args_super[SUPER_NUM_OF_PUSH_THREADS];
+    ThreadArgs thread_args[SUPER_NUM_OF_POP_THREADS];
+    
+    for (i = 0; i < SUPER_NUM_OF_PUSH_THREADS; ++i)
+    {
+        thread_args_super[i].queue = myWaitQueue;
+        thread_args_super[i].element_arr = num_arr[i];
+        thread_args_super[i].arr_size = 10;
+    }
+
+    for (i = 0; i < SUPER_NUM_OF_POP_THREADS; ++i)
+    {
+        thread_args[i].queue = myWaitQueue;
+        thread_args[i].element = 0;
+        thread_args[i].timeout = timeout;
+    }
+
+    pthread_t pop_thread_arr[SUPER_NUM_OF_POP_THREADS];
+    pthread_t push_thread_arr[SUPER_NUM_OF_PUSH_THREADS];
+
+    for (i = 0; i < SUPER_NUM_OF_PUSH_THREADS; ++i)
+    {
+        pthread_create(&push_thread_arr[i], NULL, ThreadPushRoutineSuperIMP, &thread_args_super[i]);
+    }
+
+    for (i = 0; i < SUPER_NUM_OF_POP_THREADS; ++i)
+    {
+        pthread_create(&pop_thread_arr[i], NULL, ThreadPopRoutineSuperIMP, &thread_args[i]);
+    }
+
+    for (i = 0; i < SUPER_NUM_OF_PUSH_THREADS; ++i)
+    {
+        int res;
+        void *ret_val = &res;
+        pthread_join(push_thread_arr[i], &ret_val);
+     //   std::cout << *static_cast<int *>(ret_val) << ", ";
+    }
+
+    for (i = 0; i < SUPER_NUM_OF_POP_THREADS; ++i)
+    {
+        int res;
+        void *ret_val = &res;
+        pthread_join(pop_thread_arr[i], &ret_val);
+     //   std::cout << *static_cast<int *>(ret_val) << ", ";
+    }
+    std::cout << std::endl;
+
+    PRINTTESTRESULTS("TestPushPopSuper - Pop",2, true == myWaitQueue->IsEmpty());
+
+    delete myWaitQueue;
+
+    std::cout << "\n\n";
+}
+
+void *ThreadPushRoutineIMP(void *param)
+{
+    ARGS->queue->Push(ARGS->element);
+
+    return 0;
+}
+
+void *ThreadPushRoutineSuperIMP(void *param)
+{
+     for (size_t i = 0; i < ARGS_SUPER->arr_size; ++i)
+     {
+         ARGS_SUPER->queue->Push(ARGS_SUPER->element_arr[i]);
+         sleep(1);
+     }
+
+    return 0;
+}
+
+void *ThreadPopRoutineIMP(void *param)
+{
+    ARGS->queue->Pop(ARGS->element);
+    
+    return (&(ARGS->element));
+}
+
+void *ThreadPopRoutineSuperIMP(void *param)
+{
+    for (size_t i = 0; i < SUPER_POP_ITERATIONS; ++i)
+    {
+        ARGS->queue->Pop(ARGS->element);
+        sleep(1);
+    }
+
+    return (&(ARGS->element));
+}
+
+void *ThreadPopWTimerRoutineIMP(void *param)
+{
+    ARGS->queue->Pop(ARGS->element, ARGS->timeout);
+
+    return (&(ARGS->element));
 }
