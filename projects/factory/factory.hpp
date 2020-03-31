965de83d5643c4bad2254f45c58c7c1ca73a89c3
chen.rubin <chenrubin123@gmail.com>
2020-03-31 19:19:33 +0300

    update factory before review

--- projects/factory/factory.hpp
+++ projects/factory/factory.hpp
@@ -1,7 +1,7 @@
 // Factory - Header file
-// Last update: 30/03/2020
-// Author:
-// Reviewer: 
+// Last update: 31/03/2020
+// Author: Chen
+// Reviewer: Ran
 /*----------------------------------------------------------------------------*/
 #ifndef __FACTORY_HPP__ 
 #define __FACTORY_HPP__
@@ -10,10 +10,9 @@
 #include <map> 					/* std::map */
 
 #include "MyUtils.hpp"		// class Uncopyable
-//#include "cpp_std.hpp"			// NOEXCEPT
 
-#define MAP (std::map<K, boost::function<boost::shared_ptr<T>(P)>)
-#define FUNC (boost::function<boost::shared_ptr<T>(P)>)
+#define MAP_ITERATOR typename std::map<K, boost::function<boost::shared_ptr<T>(P)> >::iterator 
+#define FUNC boost::function<boost::shared_ptr<T>(P)>
 /*----------------------------------------------------------------------------*/
 namespace ilrd
 {
@@ -24,21 +23,18 @@ template <typename T, typename K, typename P>	// T = base class
 class Factory : private Uncopyable
 {
 public:
-	/*
-	*	No Ctor and Dtor by definition
-	*/
 	// If new key replaced an old one ,return value = false
 	// If new key was immediatly inserted return value = true
 	bool AddClass(const K& key_, 
 			boost::function<boost::shared_ptr<T>(P)> createFunc_);
 	
-	boost::shared_ptr<T> Create(const K& key_, P param_) const; 
+	//	throws BadCreate and BadKey
+	boost::shared_ptr<T> Create(const K& key_, P param_) const NOEXCEPT; 
 	
 	// 2nd phase: unique_ptr c++11, 3rd? args...
 
 private:
-	std::map<K, boost::function<boost::shared_ptr<T>(P)> > m_keyFuncPairs;
-//	....
+	mutable std::map<K, boost::function<boost::shared_ptr<T>(P)> > m_keyFuncPairs;
 };
 /*----------------------------------------------------------------------------*/
 class BadCreate : public std::runtime_error
@@ -52,26 +48,54 @@ class BadKey : public std::logic_error
 public:
 	explicit BadKey();
 };
-//t = base, k = key ("hatul"), P param struct
+
 template <typename T, typename K, typename P>
 bool Factory<T,K,P>::AddClass(const K& key_, 
 			  		   boost::function<boost::shared_ptr<T>(P)> createFunc_)
 {
-	typename std::map<K, boost::function<boost::shared_ptr<T>(P)> >::iterator iter;
-//	std::pair<iter, bool> res;
-/*	res = m_keyFuncPairs.insert(std::pair<K, FUNC>(key_, createFunc_));
-	if (res == false)
+	std::pair<MAP_ITERATOR, bool> res;
+	res = m_keyFuncPairs.insert(std::pair<K, FUNC>(key_, createFunc_));
+	if (res.second == false)
 	{
 		m_keyFuncPairs[key_] = createFunc_;
 	}
-*/
+
+	return res.second;
+}
+
+template <typename T, typename K, typename P>
+boost::shared_ptr<T> Factory<T,K,P>::Create(const K& key_, P param_) const
+{
+	MAP_ITERATOR iter;
+	boost::shared_ptr<T> res;
+	iter = m_keyFuncPairs.find(key_);
+
+	if (iter == m_keyFuncPairs.end())
+	{
+		throw BadKey();
+	}
+
+	try
+	{
+		res = iter->second(param_);
+	}
+	catch(const std::exception& e)
+	{
+		throw BadCreate();
+	}
+	
 	return res;
 }
+
+factory::BadCreate::BadCreate()
+	: std::runtime_error("Bad create")
+{}
+
+factory::BadKey::BadKey()
+	: std::logic_error("Bad key")
+{}	
 /*----------------------------------------------------------------------------*/
 } // namespace factory
-
-
-
 } // namespace ilrd
 /*----------------------------------------------------------------------------*/
 #endif // __FACTORY_HPP__
--- projects/factory/factory_tests.cpp
+++ projects/factory/factory_tests.cpp
@@ -1,8 +1,122 @@
+#include <boost/shared_ptr.hpp> /* shared_ptr */
+
 #include "factory.hpp"
+#include "MyUtils.hpp"
 
 using namespace ilrd;
+using namespace factory;
+
+class Animal
+{
+public:
+    Animal(); 
+    virtual ~Animal();
+    size_t GetNumOfLegs();
+    virtual bool GetTailStatus();
+
+private:
+    size_t m_numOfLegs;
+};
+
+Animal::Animal()
+{}
+
+Animal::~Animal()
+{}
+
+bool Animal::GetTailStatus()
+{
+    return 0;
+}
+
+class Dog : public Animal
+{
+public:
+    Dog(size_t numOflegs_, bool tailStatus);
+    virtual ~Dog();
+    void GetNumOfLegs();
+    virtual bool GetTailStatus();
+
+private:
+    size_t m_numOfLegs;
+    bool m_IsWavingTail;
+};
+
+Dog::Dog(size_t numOflegs_, bool tailStatus_)
+    : m_numOfLegs(numOflegs_)
+    , m_IsWavingTail(tailStatus_)
+{}
+
+Dog::~Dog()
+{}
+
+bool Dog::GetTailStatus()
+{
+    return m_IsWavingTail;
+}
+
+struct DogStruct
+{
+    size_t m_numOfLegs;
+    bool m_IsWavingTail;
+};
+
+boost::shared_ptr<Animal> AddDog(DogStruct *ds);
+boost::shared_ptr<Animal> AddDogExtra(DogStruct *ds);
+
+void TestCreateAndAdd();
 
 int main()
 {
+    TestCreateAndAdd();
+
     return 0;
+}
+
+void TestCreateAndAdd()
+{
+    bool status = false;
+
+    DogStruct ds_happy;
+    ds_happy.m_IsWavingTail = true;
+    ds_happy.m_numOfLegs = 4;
+
+    DogStruct ds_sad;
+    ds_sad.m_IsWavingTail = false;
+    ds_sad.m_numOfLegs = 4;
+
+    Factory<Animal, std::string, DogStruct *> fact;
+    status = fact.AddClass("HappyDog", AddDog);
+    PRINTTESTRESULTS("TestCreateAndAdd",1, true == status);
+    status = fact.AddClass("SadDog", AddDog);
+    PRINTTESTRESULTS("TestCreateAndAdd",2, true == status);
+
+    boost::shared_ptr<Animal> animal_ptr1 = fact.Create("HappyDog", &ds_happy);
+    boost::shared_ptr<Animal> animal_ptr2 = fact.Create("SadDog", &ds_sad);
+
+    PRINTTESTRESULTS("TestCreateAndAdd",1, true == animal_ptr1->GetTailStatus());
+    PRINTTESTRESULTS("TestCreateAndAdd",2, false == animal_ptr2->GetTailStatus());
+    
+    status = fact.AddClass("HappyDog", AddDogExtra);
+    PRINTTESTRESULTS("TestCreateAndAdd",1, false == status);
+    boost::shared_ptr<Animal> animal_ptr3 = fact.Create("HappyDog", &ds_happy);
+    PRINTTESTRESULTS("TestCreateAndAdd",1, true == animal_ptr1->GetTailStatus());
+
+//  this should fail in compile 
+ // boost::shared_ptr<Animal> animal_ptr4 = fact.Create("HappySpider", &ds_happy);
+}
+
+boost::shared_ptr<Animal> AddDog(DogStruct *ds)
+{
+    boost::shared_ptr<Animal> shared_ptr(new Dog(ds->m_numOfLegs, ds->m_IsWavingTail));
+
+    return shared_ptr;
+}
+
+boost::shared_ptr<Animal> AddDogExtra(DogStruct *ds)
+{
+    boost::shared_ptr<Animal> shared_ptr(new Dog(ds->m_numOfLegs, ds->m_IsWavingTail));
+    std::cout << "I am the best dog ever" << std::endl;
+
+    return shared_ptr;
 }
